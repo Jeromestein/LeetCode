76c000507f9acaa60dce83774c11515b1705b62d
Jerome <nietsemorej@gmail.com>
2021-05-22 21:12:00 +0800

    HashMap-O(n)?

---
+++ 1.two-sum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x)) {
+                return new int[] { map.get(x), i };
+            }
+
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Solution");
+    }
+}
+// @lc code=end
