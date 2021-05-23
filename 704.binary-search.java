51b43da9cc43a8f346fe8246fa61c708a077d1da
Jerome <nietsemorej@gmail.com>
2021-05-23 23:17:30 +0800

    Avoid overflow: pivot = left + (right - left) / 2

--- 704.binary-search.java
+++ 704.binary-search.java
@@ -10,7 +10,7 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int pivot = (left + right) / 2;
+            int pivot = left + (right - left) / 2;
             if (nums[pivot] == target) {
                 return pivot;
             } else if (nums[pivot] > target) {
