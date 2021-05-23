2f1dfdf69e34c03866839f8b405f07b61493d3f0
Jerome <nietsemorej@gmail.com>
2021-05-23 23:03:25 +0800

    Optimize code: while(left <= right)

--- 704.binary-search.java
+++ 704.binary-search.java
@@ -8,32 +8,18 @@
 // @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
-        int front = 0, rear = nums.length - 1;
-        while (front != rear) {
-            int index = (front + rear) / 2;
-            if (nums[index] == target) {
-                return index;
-            } else if (nums[index] > target) {
-                if (index > 0) {
-                    rear = index - 1;
-                } else {
-                    rear = 0;
-                }
-            } else if (nums[index] < target) {
-                if (front < nums.length - 1) {
-                    front = index + 1;
-                } else {
-                    front = nums.length - 1;
-                }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int pivot = (left + right) / 2;
+            if (nums[pivot] == target) {
+                return pivot;
+            } else if (nums[pivot] > target) {
+                right = pivot - 1;
+            } else if (nums[pivot] < target) {
+                left = pivot + 1;
             }
         }
-        if (front == rear) {
-            if (nums[front] == target)
-                return front;
-            else
-                return -1;
-        }
-        throw new IllegalArgumentException("No Solution");
+        return -1;
     }
 }
 // @lc code=end
