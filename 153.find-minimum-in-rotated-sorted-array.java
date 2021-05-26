fbaf925b6ab356cb85d6debbfe1f17ed50319a4e
Jerome <nietsemorej@gmail.com>
2021-05-26 09:18:28 +0800

    Add the explanation of updating the search space boundaries

--- 153.find-minimum-in-rotated-sorted-array.java
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -5,6 +5,20 @@
  */
 
 // @lc code=start
+
+/*
+Here is an intuition for updating the search space boundaries:
+
+If nums[mid] < nums[hi], 
+nums[mid] or some value before it could be our inflection point. 
+Therefore, let hi = mid, 
+including mid in our new search space.
+
+If nums[mid] > nums[hi], 
+nums[mid] cannot be our inflection point. 
+Candidates are to the right of it. 
+Let lo = mid + 1, exluding the mid from the search space.
+*/
 class Solution {
     public int findMin(int[] nums) {
         int lo = 0, hi = nums.length - 1;
