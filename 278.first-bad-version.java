e1dc6ec3021ce50331f1356ede2849148f87ee93
Jerome <nietsemorej@gmail.com>
2021-05-27 08:59:42 +0800

    AC, 97.85%, 14.72%. Modified Binary Seach, like findMin().

---
+++ 278.first-bad-version.java
@@ -0,0 +1,28 @@
+import jdk.internal.vm.compiler.word.Pointer;
+
+/*
+ * @lc app=leetcode id=278 lang=java
+ *
+ * [278] First Bad Version
+ */
+
+// @lc code=start
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int pivot = left + (right - left) / 2;
+            if (isBadVersion(pivot) == false) {
+                left = pivot + 1;
+            } else {
+                right = pivot;
+            }
+        }
+
+        return left;
+    }
+}
+// @lc code=end
