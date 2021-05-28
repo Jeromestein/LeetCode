4f43496de4dca7d94bce21fc62461d638257b4f1
Jerome <nietsemorej@gmail.com>
2021-05-28 16:27:45 +0800

    New Method, in most cases O(logn), wosrt cases O(n).

--- 81.search-in-rotated-sorted-array-ii.java
+++ 81.search-in-rotated-sorted-array-ii.java
@@ -8,37 +8,36 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-        // findMIn()
-        // while (left < right) {
-        // int pivot = left + (right - left) / 2;
-        // if (nums[pivot] <= nums[right]) {
-        // right = pivot;
-        // } else {
-        // left = pivot + 1;
-        // }
-        // }
-        int rot = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < nums[i - 1]) {
-                rot = i;
-                break;
+
+        // We always get a sorted part and a half part
+        // we can check sorted part to decide where to go next
+        while (left <= right) {
+            int pivot = left + (right - left) / 2;
+            if (nums[pivot] == target) {
+                return true;
+            } else if (nums[left] < nums[pivot]) {
+                // left part is sorted
+                if (target < nums[left] || target > nums[pivot]) {
+                    left = pivot + 1;
+                } else {
+                    right = pivot - 1;
+                }
+            } else if (nums[left] > nums[pivot]) {
+                // right part is rotated
+                if (target < nums[pivot] || target > nums[right]) {
+                    right = pivot - 1;
+                } else {
+                    left = pivot + 1;
+                }
+            } else {
+                // duplicates, we know nums[pivot]=nums[left]=nums[right], but != target
+                // based on current information, we can only move left pointer to skip one cell
+                // thus in the worest case, we would have target: 2, and array like 11111111,
+                // then the running time would be O(n)
+                left++;
             }
         }
 
-        // int rot = left;
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
-            int pivot = left + (right - left) / 2;
-            int mid = (pivot + rot) % nums.length;
-            if (target == nums[mid]) {
-                return true;
-            } else if (target < nums[mid]) {
-                right = pivot - 1;
-            } else {
-                left = pivot + 1;
-            }
-        }
         return false;
     }
 }
