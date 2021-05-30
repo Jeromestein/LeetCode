924a16df4b7dd92669b28a8aeb7c79e098ec2ac3
Jerome <nietsemorej@gmail.com>
2021-05-30 22:38:15 +0800

    Make it clean and consice.

--- 74.search-a-2-d-matrix.java
+++ 74.search-a-2-d-matrix.java
@@ -7,16 +7,16 @@
 // @lc code=start
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int lo = 0, hi = matrix[0].length * matrix.length - 1;
+        int rowNum = matrix.length, colNum = matrix[0].length;
+        int lo = 0, hi = rowNum * colNum - 1;
         while (lo <= hi) {
             int pivot = lo + (hi - lo) / 2;
-            int i = pivot / matrix[0].length;
-            int j = pivot % matrix[0].length;
-            if (matrix[i][j] == target) {
+            int pivotVal = matrix[pivot / colNum][pivot % colNum];
+            if (pivotVal == target) {
                 return true;
-            } else if (target > matrix[i][j]) {
+            } else if (target > pivotVal) {
                 lo = pivot + 1;
-            } else if (target < matrix[i][j]) {
+            } else if (target < pivotVal) {
                 hi = pivot - 1;
             }
         }
