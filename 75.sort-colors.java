cc6e5333c1f204731ba5f0fe306cff01bfe45118
Jerome <nietsemorej@gmail.com>
2021-06-04 15:43:50 +0800

    one pass

--- 75.sort-colors.java
+++ 75.sort-colors.java
@@ -7,31 +7,45 @@
 // @lc code=start
 class Solution {
     public void sortColors(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
-        // return nums;
-    }
+        // quickSort(nums, 0, nums.length - 1);
 
-    public void quickSort(int[] nums, int l, int r) {
-        if (l >= r)
-            return;
-
-        int mid = partition(nums, l, r);
-        quickSort(nums, l, mid);
-        quickSort(nums, mid + 1, r);
-    }
-
-    public int partition(int[] nums, int l, int r) {
-        int pivot = nums[l];
-        while (l < r) {
-            while (l < r && pivot <= nums[r])
-                r--;
-            nums[l] = nums[r];
-            while (l < r && pivot >= nums[l])
-                l++;
-            nums[r] = nums[l];
+        int pointer[] = { 0, 0, nums.length - 1 };
+        for (int i = 0; i <= pointer[2]; i++) {
+            if (nums[i] == 0) {
+                swap(nums, pointer[0]++, i);
+            } else if (nums[i] == 2) {
+                swap(nums, pointer[2]--, i--);
+            }
         }
-        nums[l] = pivot;
-        return l;
     }
+
+    public void swap(int[] A, int a, int b) {
+        int tmp = A[a];
+        A[a] = A[b];
+        A[b] = tmp;
+    }
+
+    // public void quickSort(int[] nums, int l, int r) {
+    // if (l >= r)
+    // return;
+
+    // int mid = partition(nums, l, r);
+    // quickSort(nums, l, mid);
+    // quickSort(nums, mid + 1, r);
+    // }
+
+    // public int partition(int[] nums, int l, int r) {
+    // int pivot = nums[l];
+    // while (l < r) {
+    // while (l < r && pivot <= nums[r])
+    // r--;
+    // nums[l] = nums[r];
+    // while (l < r && pivot >= nums[l])
+    // l++;
+    // nums[r] = nums[l];
+    // }
+    // nums[l] = pivot;
+    // return l;
+    // }
 }
 // @lc code=end
