d33ca51c1f65b57a66f6c74c82d5b7e8e2f168f2
Jerome <nietsemorej@gmail.com>
2021-06-08 17:51:53 +0800

    Two ways

--- 349.intersection-of-two-arrays.java
+++ 349.intersection-of-two-arrays.java
@@ -31,23 +31,25 @@ class Solution {
         }
         return res;
     }
-
-    // public int[] intersection(int[] nums1, int[] nums2) {
-    // HashSet<Integer> set1 = new HashSet<Integer>();
-    // for (Integer n : nums1)
-    // set1.add(n);
-    // HashSet<Integer> set2 = new HashSet<Integer>();
-    // for (Integer n : nums2)
-    // set2.add(n);
-
-    // // get the intersections
-    // set1.retainAll(set2);
-
-    // int[] output = new int[set1.size()];
-    // int idx = 0;
-    // for (int s : set1)
-    // output[idx++] = s;
-    // return output;
-    // }
 }
+
+// class Solution {
+// public int[] intersection(int[] nums1, int[] nums2) {
+// HashSet<Integer> set1 = new HashSet<Integer>();
+// for (Integer n : nums1)
+// set1.add(n);
+// HashSet<Integer> set2 = new HashSet<Integer>();
+// for (Integer n : nums2)
+// set2.add(n);
+
+// // get the intersections
+// set1.retainAll(set2);
+
+// int[] output = new int[set1.size()];
+// int idx = 0;
+// for (int s : set1)
+// output[idx++] = s;
+// return output;
+// }
+// }
 // @lc code=end
