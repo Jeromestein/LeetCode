263d1ed3e50a914201b59b47c877e18f4f250f82
Jerome <nietsemorej@gmail.com>
2021-06-08 17:02:00 +0800

    Add comments

--- 347.top-k-frequent-elements.java
+++ 347.top-k-frequent-elements.java
@@ -23,12 +23,16 @@ class Solution {
             }
         }
 
+        // get element index
+        // !! so the Space will be save!
+        // and all the index will be positive !!!
         int[] count = new int[max - min + 1];
         for (int num : nums) {
             count[num - min]++;
         }
 
         // O(n*log(k))
+        // to find the k element index
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
         for (int i = 0; i < count.length; i++) {
             if (count[i] > 0) {
@@ -48,8 +52,10 @@ class Solution {
         int[] res = new int[k];
         int i = 0;
         // O(n)
+        // You may return the answer in any order.
         for (int ix = 0; ix < count.length; ix++) {
             if (count[ix] > 0 && count[ix] >= pq.peek()) {
+
                 res[i] = ix + min;
                 i++;
                 if (i == k) {
@@ -61,95 +67,5 @@ class Solution {
         // O(n * (2 + log(k)))
         return res;
     }
-    // int[] unique;
-    // Map<Integer, Integer> count;
-
-    // public void swap(int a, int b) {
-    // int tmp = unique[a];
-    // unique[a] = unique[b];
-    // unique[b] = tmp;
-    // }
-
-    // public int partition(int left, int right, int pivot_index) {
-    // int pivot_frequency = count.get(unique[pivot_index]);
-    // // 1. move pivot to end
-    // swap(pivot_index, right);
-    // int store_index = left;
-
-    // // 2. move all less frequent elements to the left
-    // for (int i = left; i <= right; i++) {
-    // if (count.get(unique[i]) < pivot_frequency) {
-    // swap(store_index, i);
-    // store_index++;
-    // }
-    // }
-
-    // // 3. move pivot to its final place
-    // swap(store_index, right);
-
-    // return store_index;
-    // }
-
-    // public void quickselect(int left, int right, int k_smallest) {
-    // /*
-    // * Sort a list within left..right till kth less frequent element takes its
-    // * place.
-    // */
-
-    // // base case: the list contains only one element
-    // if (left == right)
-    // return;
-
-    // // select a random pivot_index
-    // Random random_num = new Random();
-    // int pivot_index = left + random_num.nextInt(right - left);
-
-    // // find the pivot position in a sorted list
-    // pivot_index = partition(left, right, pivot_index);
-
-    // // if the pivot is in its final sorted position
-    // if (k_smallest == pivot_index) {
-    // return;
-    // } else if (k_smallest < pivot_index) {
-    // // go left
-    // quickselect(left, pivot_index - 1, k_smallest);
-    // } else {
-    // // go right
-    // quickselect(pivot_index + 1, right, k_smallest);
-    // }
-    // }
-
-    // public int[] topKFrequent(int[] nums, int k) {
-    // // build hash map : character and how often it appears
-    // count = new HashMap();
-    // for (int num : nums) {
-    // // Map.getOrDefault(Object key, V defaultValue);
-    // // 如果在Map中存在key，则返回key所对应的的value。
-    // // If Map has the key, then return value
-    // // If no key, then return defaultValue
-    // count.put(num, count.getOrDefault(num, 0) + 1);
-    // }
-
-    // // array of unique elements
-    // int n = count.size();
-    // unique = new int[n];
-    // int i = 0;
-    // // for (int num : count.keySet()) {
-    // // unique[i] = num;
-    // // i++;
-    // // }
-    // for (Map.Entry<Integer, Integer> num : count.entrySet()) {
-    // unique[i++] = num.getKey();
-    // }
-
-    // // kth top frequent element is (n - k)th less frequent.
-    // // Do a partial sort: from less frequent to the most frequent, till
-    // // (n - k)th less frequent element takes its place (n - k) in a sorted array.
-    // // All element on the left are less frequent.
-    // // All the elements on the right are more frequent.
-    // quickselect(0, n - 1, n - k);
-    // // Return top k frequent elements
-    // return Arrays.copyOfRange(unique, n - k, n);
-    // }
 }
 // @lc code=end
