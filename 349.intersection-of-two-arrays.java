d949bfbc2afe179585edf2a9f9dc7ff3a9843a4c
Jerome <nietsemorej@gmail.com>
2021-06-08 17:41:52 +0800

    AC, 100%, 86.48%. ArrayList & boolean[]

--- 349.intersection-of-two-arrays.java
+++ 349.intersection-of-two-arrays.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,21 +12,42 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set1 = new HashSet<Integer>();
-        for (Integer n : nums1)
-            set1.add(n);
-        HashSet<Integer> set2 = new HashSet<Integer>();
-        for (Integer n : nums2)
-            set2.add(n);
+        boolean[] flag = new boolean[1001];
+        for (int num : nums1) {
+            flag[num] = true;
+        }
 
-        // get the intersections
-        set1.retainAll(set2);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (flag[num] == true) {
+                list.add(num);
+                flag[num] = false;
+            }
+        }
+        int[] res = new int[list.size()];
 
-        int[] output = new int[set1.size()];
-        int idx = 0;
-        for (int s : set1)
-            output[idx++] = s;
-        return output;
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
     }
+
+    // public int[] intersection(int[] nums1, int[] nums2) {
+    // HashSet<Integer> set1 = new HashSet<Integer>();
+    // for (Integer n : nums1)
+    // set1.add(n);
+    // HashSet<Integer> set2 = new HashSet<Integer>();
+    // for (Integer n : nums2)
+    // set2.add(n);
+
+    // // get the intersections
+    // set1.retainAll(set2);
+
+    // int[] output = new int[set1.size()];
+    // int idx = 0;
+    // for (int s : set1)
+    // output[idx++] = s;
+    // return output;
+    // }
 }
 // @lc code=end
