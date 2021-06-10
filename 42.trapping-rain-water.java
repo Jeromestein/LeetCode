988be1385cd526fbde77217e69cbcfbbe129ae3f
Jerome <nietsemorej@gmail.com>
2021-06-10 20:43:41 +0800

    AC, 1ms, 84.07%, 99.65%

--- 42.trapping-rain-water.java
+++ 42.trapping-rain-water.java
@@ -7,39 +7,24 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
-        // level, bottom, start, end
-        int start = 0, end = 0;
-        int level, bottom, dis;
-        int sumWater = 0, sumheight = 0;
-
-        while (start < height.length) {
-            // find a new valley start: decrease
-            if (start < height.length - 1 && height[start] > height[start + 1]) {
-                end = start;
-                sumheight = 0;
-
-                // find valley bottom
-                while (end < height.length - 1 && height[end] > height[end + 1]) {
-                    end++;
-                    sumheight += height[end];
-                }
-                if (end < height.length - 1 && height[end] < height[end + 1]) {
-                    // find valley end
-                    while (end < height.length - 1 && height[end] < height[end + 1]) {
-                        end++;
-                        sumheight += height[end];
-                    }
-                    // now we get valley start and end
-                    sumheight -= height[end];
-                    level = Math.min(height[start], height[end]);
-                    dis = end - start - 1;
-                    // sumWater
-                    sumWater += level * dis - sumheight;
-                }
+        int a = 0;
+        int b = height.length - 1;
+        int max = 0;
+        int leftmax = 0;
+        int rightmax = 0;
+        while (a <= b) {
+            leftmax = Math.max(leftmax, height[a]);
+            rightmax = Math.max(rightmax, height[b]);
+            if (leftmax < rightmax) {
+                max += (leftmax - height[a]); // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be
+                                              // stored
+                a++;
+            } else {
+                max += (rightmax - height[b]);
+                b--;
             }
-            start = Math.max(end, start + 1);
         }
-        return sumWater;
+        return max;
     }
 }
 // @lc code=end
