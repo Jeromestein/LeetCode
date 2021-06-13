5831d006ca22e3b2d1651edfd3d0186ba095080b
Jerome <nietsemorej@gmail.com>
2021-06-13 13:48:40 +0800

    AC, 0ms, 100%, 81.13%. 2 pointers, runner & walker.

--- 141.linked-list-cycle.java
+++ 141.linked-list-cycle.java
@@ -13,13 +13,18 @@ import java.util.HashMap;
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        while (head != null) {
-            map.put(head, map.getOrDefault(head, 0) + 1);
-            if (map.get(head) > 1) {
+        if (head == null)
+            return false;
+        // 1. Use two pointers, walker and runner.
+        // 2. walker moves step by step. runner moves two steps at time.
+        // 3. if a cycle exists, walker and runner will meet at some point.
+        ListNode walker = head;
+        ListNode runner = head;
+        while (runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner)
                 return true;
-            }
-            head = head.next;
         }
         return false;
     }
