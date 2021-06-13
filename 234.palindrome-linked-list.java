132674a0e548699583408d40c52228a36b5e7ff3
Jerome <nietsemorej@gmail.com>
2021-06-13 14:53:07 +0800

    AC, 4ms, 97.69%, 93.49%. 2 pointers and reverse linklist

--- 234.palindrome-linked-list.java
+++ 234.palindrome-linked-list.java
@@ -14,38 +14,71 @@ import java.util.Stack;
  * ListNode next) { this.val = val; this.next = next; } }
  */
 class Solution {
+    // public boolean isPalindrome(ListNode head) {
+    // // 1. get the length
+    // int length = 0;
+    // for (ListNode p = head; p != null; p = p.next) {
+    // length++;
+    // }
+
+    // Stack<Integer> stack = new Stack<>();
+    // ListNode p = head;
+    // for (int i = 0; i < length / 2; i++) {
+    // stack.push(p.val);
+    // p = p.next;
+    // }
+
+    // if (length % 2 == 1) {
+    // p = p.next;
+    // }
+
+    // for (int i = (length + 1) / 2; i < length; i++) {
+    // if (stack.peek() == p.val) {
+    // stack.pop();
+    // p = p.next;
+    // } else {
+    // return false;
+    // }
+    // }
+
+    // if (stack.empty() == true) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
     public boolean isPalindrome(ListNode head) {
-        // 1. get the length
-        int length = 0;
-        for (ListNode p = head; p != null; p = p.next) {
-            length++;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
-
-        Stack<Integer> stack = new Stack<>();
-        ListNode p = head;
-        for (int i = 0; i < length / 2; i++) {
-            stack.push(p.val);
-            p = p.next;
+        // odd nodes: let right half smaller
+        if (fast != null) {
+            slow = slow.next;
         }
+        slow = reverse(slow);
+        fast = head;
 
-        if (length % 2 == 1) {
-            p = p.next;
-        }
-
-        for (int i = (length + 1) / 2; i < length; i++) {
-            if (stack.peek() == p.val) {
-                stack.pop();
-                p = p.next;
-            } else {
+        while (slow != null) {
+            if (fast.val != slow.val) {
                 return false;
             }
+            fast = fast.next;
+            slow = slow.next;
         }
+        return true;
+    }
 
-        if (stack.empty() == true) {
-            return true;
-        } else {
-            return false;
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
         }
+        return prev;
     }
 }
 // @lc code=end
