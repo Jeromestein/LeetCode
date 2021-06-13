5ef7cb57193153638ab146c8cd46946c171bb284
Jerome <nietsemorej@gmail.com>
2021-06-13 15:21:51 +0800

    AC, 0ms, 100%, 85.72%. X2 pointers, create a new list acutally!!!

---
+++ 328.odd-even-linked-list.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=328 lang=java
+ *
+ * [328] Odd Even Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head != null) {
+
+            ListNode odd = head, even = head.next, evenHead = even;
+
+            // create a new list acutally!!!
+            while (even != null && even.next != null) {
+                // change the pointer!!!
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+        }
+        return head;
+    }
+}
+// @lc code=end
