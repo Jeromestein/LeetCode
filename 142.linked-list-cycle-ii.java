757a99bdd487671272cf0f3783913fda2ec972f8
Jerome <nietsemorej@gmail.com>
2021-06-14 17:17:00 +0800

    a shorter solution, s+m=k=nr.

--- 142.linked-list-cycle-ii.java
+++ 142.linked-list-cycle-ii.java
@@ -10,58 +10,76 @@
  * ListNode(int x) { val = x; next = null; } }
  */
 public class Solution {
+    // public ListNode detectCycle(ListNode head) {
+    // if (head == null)
+    // return null;
+    // // 1. is there a cycle?
+    // boolean flag = false;
+    // ListNode runner = head, walker = head, end = head;
+    // while (runner.next != null && runner.next.next != null) {
+    // runner = runner.next.next;
+    // walker = walker.next;
+    // if (runner == walker) {
+    // flag = true;
+    // end = runner;
+    // // System.out.println(end.val);
+    // break;
+    // }
+    // }
+    // if (flag == false)
+    // return null;
+
+    // // 2. break cycle at end node
+    // // consider it as find the intersection of two lists
+
+    // // 2.1 get the lengths
+    // ListNode head1 = head, head2 = end.next;
+    // int len1 = 0, len2 = 0;
+    // while (head1 != end) {
+    // len1++;
+    // head1 = head1.next;
+    // }
+    // while (head2 != end) {
+    // len2++;
+    // head2 = head2.next;
+    // }
+
+    // // 2.2 set the start node
+    // head1 = head;
+    // head2 = end.next;
+    // while (len1 > len2) {
+    // len1--;
+    // head1 = head1.next;
+    // }
+    // while (len2 > len1) {
+    // len2--;
+    // head2 = head2.next;
+    // }
+
+    // // 2.3 find the intersection
+    // while (head1 != head2) {
+    // head1 = head1.next;
+    // head2 = head2.next;
+    // }
+    // return head1;
+    // }
+
     public ListNode detectCycle(ListNode head) {
-        if (head == null)
+        if (head == null || head.next == null)
             return null;
-        // 1. is there a cycle?
-        boolean flag = false;
-        ListNode runner = head, walker = head, end = head;
-        while (runner.next != null && runner.next.next != null) {
-            runner = runner.next.next;
-            walker = walker.next;
-            if (runner == walker) {
-                flag = true;
-                end = runner;
-                // System.out.println(end.val);
-                break;
+        ListNode slow = head, fast = head, start = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (slow != start) {
+                    slow = slow.next;
+                    start = start.next;
+                }
+                return start;
             }
         }
-        if (flag == false)
-            return null;
-
-        // 2. break cycle at end node
-        // consider it as find the intersection of two lists
-
-        // 2.1 get the lengths
-        ListNode head1 = head, head2 = end.next;
-        int len1 = 0, len2 = 0;
-        while (head1 != end) {
-            len1++;
-            head1 = head1.next;
-        }
-        while (head2 != end) {
-            len2++;
-            head2 = head2.next;
-        }
-
-        // 2.2 set the start node
-        head1 = head;
-        head2 = end.next;
-        while (len1 > len2) {
-            len1--;
-            head1 = head1.next;
-        }
-        while (len2 > len1) {
-            len2--;
-            head2 = head2.next;
-        }
-
-        // 2.3 find the intersection
-        while (head1 != head2) {
-            head1 = head1.next;
-            head2 = head2.next;
-        }
-        return head1;
+        return null;
     }
 }
 // @lc code=end
