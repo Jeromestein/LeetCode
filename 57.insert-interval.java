f7a13d013ae1e211aaee21ff7ea2aa8a6ae77f05
Jerome <nietsemorej@gmail.com>
2021-06-15 19:54:57 +0800

    AC, 1ms, 98.64%, 22.5%.

---
+++ 57.insert-interval.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=57 lang=java
+ *
+ * [57] Insert Interval
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList();
+
+        int i = 0;
+        // add all the intervals ending before newInterval starts
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
+        }
+        // merge all overlapping intervals to one considering newInterval
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            // we could mutate newInterval here also
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        res.add(newInterval);
+
+        // add all the rest
+        while (i < intervals.length) {
+            res.add(intervals[i]);
+            i++;
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+
+}
+// @lc code=end
