5e8fab6a33944657801b20c765ddb255bbc96008
Jerome <nietsemorej@gmail.com>
2021-06-17 22:13:22 +0800

    Add comments

--- 3.longest-substring-without-repeating-characters.java
+++ 3.longest-substring-without-repeating-characters.java
@@ -13,12 +13,20 @@ class Solution {
         if (s.length() < 2) {
             return s.length();
         }
+        // 2 pointers
         int start, end;
         start = end = 0;
+        // the input is char type, so new boolean[256] is good enough
+        // dont need hashmap
+        // use O(1) space
         boolean[] isRepeated = new boolean[256];
         int res = 0;
         while (end < s.length()) {
             while (isRepeated[s.charAt(end)] == true) {
+                // if it is repeated
+                // then move forward start pointer one by one
+                // and reset isRepeated as false for the elements that start walks through
+                // runs in O(n) time
                 isRepeated[s.charAt(start)] = false;
                 start++;
             }
