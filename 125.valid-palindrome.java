d86f96a779ad70285561db5c1f3d47b0826bca03
Jerome <nietsemorej@gmail.com>
2021-06-17 21:07:56 +0800

    AC, my code, 68%, 89%.

---
+++ 125.valid-palindrome.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toUpperCase();
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
