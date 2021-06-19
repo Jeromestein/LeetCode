d5327a6e4aaeca912ebdd09892fff9d6c9c39fcc
Jerome <nietsemorej@gmail.com>
2021-06-19 15:16:04 +0800

    Add comments

--- 76.minimum-window-substring.java
+++ 76.minimum-window-substring.java
@@ -16,22 +16,29 @@ class Solution {
         int left = 0, right = 0;
         int ansL = -1, ansR = -1, len = Integer.MAX_VALUE;
         while (right < sChar.length) {
+
             need[sChar[right]]--;
             if (need[sChar[right]] >= 0) {
                 needCharCNT--;
             }
+
+            // try to make this substring shorter in this loop
             while (needCharCNT == 0) {
                 if (right - left < len) {
+                    // save the current shortest substring
                     ansL = left;
                     ansR = right;
                     len = ansR - ansL;
                 }
+
+                // restore before left+1
                 need[sChar[left]]++;
                 if (need[sChar[left]] > 0) {
                     needCharCNT++;
                 }
                 left++;
             }
+
             right++;
         }
         return ansL == -1 ? "" : s.substring(ansL, ansR + 1);
