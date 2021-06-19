f47d3609b9b1901a5ce61b1f5e4e6014c574cca1
Jerome <nietsemorej@gmail.com>
2021-06-19 16:10:24 +0800

    Add a binary search solution

--- 209.minimum-size-subarray-sum.java
+++ 209.minimum-size-subarray-sum.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode id=209 lang=java
  *
@@ -6,6 +8,7 @@
 
 // @lc code=start
 class Solution {
+    // 2 Pointers
     public int minSubArrayLen(int target, int[] nums) {
         int left = 0, right = 0;
         int sum = 0;
@@ -24,5 +27,33 @@ class Solution {
 
         return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
     }
+
+    // Binary Search
+    // elements must be non negtive!!!
+    // public int minSubArrayLen(int target, int[] nums) {
+    // int minLen = Integer.MAX_VALUE;
+
+    // // size = n+1 for easier calculations
+    // // sums[0]=0 : Meaning that it is the sum of first 0 elements
+    // // sums[1]=A[0] : Sum of first 1 elements
+    // // ans so on...
+    // int[] sums = new int[nums.length + 1];
+    // sums[0] = 0;
+    // for (int i = 1; i < sums.length; i++) {
+    // sums[i] = sums[i - 1] + nums[i - 1];
+    // }
+
+    // for (int i = 1; i < sums.length; i++) {
+    // int bound = Arrays.binarySearch(sums, target + sums[i - 1]);
+    // if (bound < 0) {
+    // bound = -bound - 1;
+    // }
+    // if (bound <= nums.length) {
+    // minLen = Math.min(minLen, bound - (i - 1));
+    // }
+    // }
+
+    // return (minLen == Integer.MAX_VALUE) ? 0 : minLen;
+    // }
 }
 // @lc code=end
