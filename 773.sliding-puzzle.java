5dade7dd8cbff04f269c310b41a7be8bebc37628
Jerome <nietsemorej@gmail.com>
2021-07-04 16:21:40 +0800

    4ms, 99%, 70%.

---
+++ 773.sliding-puzzle.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode id=773 lang=java
+ *
+ * [773] Sliding Puzzle
+ */
+
+// @lc code=start
+class Solution {
+    int[][] neighbors = { { 1, 3 }, { 0, 2, 4 }, { 1, 5 }, { 0, 4 }, { 1, 3, 5 }, { 2, 4 } };
+
+    public int slidingPuzzle(int[][] board) {
+        StringBuffer sb = new StringBuffer();
+        // matrix to array
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                sb.append(board[i][j]);
+            }
+        }
+        String initial = sb.toString();
+        if ("123450".equals(initial)) {
+            return 0;
+        }
+
+        int step = 0;
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(initial);
+        Set<String> seen = new HashSet<String>();
+        seen.add(initial);
+
+        while (!queue.isEmpty()) {
+            ++step;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                String status = queue.poll();
+                for (String nextStatus : oneMoveStates(status)) {
+                    if (!seen.contains(nextStatus)) {
+                        if ("123450".equals(nextStatus)) {
+                            return step;
+                        }
+                        queue.offer(nextStatus);
+                        seen.add(nextStatus);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // enumerate all the status we can get through one move
+    // A move consists of
+    // choosing 0 and a 4-directionally adjacent number and swapping it.
+    public List<String> oneMoveStates(String status) {
+        List<String> ret = new ArrayList<String>();
+        char[] array = status.toCharArray();
+        int x = status.indexOf('0');
+        for (int y : neighbors[x]) {
+            swap(array, x, y);
+            ret.add(new String(array));
+            swap(array, x, y);
+        }
+        return ret;
+    }
+
+    public void swap(char[] array, int x, int y) {
+        char temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
+    }
+}
+// @lc code=end
