3e6009379272eb9b1547e8d07466434dd46d1231
Jerome <nietsemorej@gmail.com>
2021-07-04 20:16:32 +0800

    25ms, 89%, 44%. hashmap, bfs, recursion.

---
+++ 133.clone-graph.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=133 lang=java
+ *
+ * [133] Clone Graph
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private HashMap<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
+        }
+        // use recursion to build the graph deep copy
+        // so if a node is visited, then we return the copy from hashmap
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        // copy the node,
+        // to implement DEEP copy, we should not copy the neighbors,
+        // but creat a new one
+        Node cloneNode = new Node(node.val, new ArrayList());
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors) {
+            // recursion
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
+    }
+}
+
+// class Solution {
+// public Node cloneGraph(Node node) {
+// if (node == null) {
+// return node;
+// }
+
+// HashMap<Node, Node> visited = new HashMap();
+
+// // 将题目给定的节点添加到队列
+// LinkedList<Node> queue = new LinkedList<Node>();
+// queue.add(node);
+// // 克隆第一个节点并存储到哈希表中
+// visited.put(node, new Node(node.val, new ArrayList()));
+
+// // 广度优先搜索
+// while (!queue.isEmpty()) {
+// // 取出队列的头节点
+// Node n = queue.remove();
+// // 遍历该节点的邻居
+// for (Node neighbor : n.neighbors) {
+// if (!visited.containsKey(neighbor)) {
+// // 如果没有被访问过，就克隆并存储在哈希表中
+// visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
+// // 将邻居节点加入队列中
+// queue.add(neighbor);
+// }
+// // 更新当前节点的邻居列表
+// visited.get(n).neighbors.add(visited.get(neighbor));
+// }
+// }
+
+// return visited.get(node);
+// }
+// }
+
+// @lc code=end
