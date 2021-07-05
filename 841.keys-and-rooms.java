86bada8aeca6bcb5f0e1d4851ed21cbee80c3971
Jerome <nietsemorej@gmail.com>
2021-07-05 20:19:26 +0800

    dfs

--- 841.keys-and-rooms.java
+++ 841.keys-and-rooms.java
@@ -9,27 +9,48 @@ import java.util.Queue;
  */
 
 // @lc code=start
+// class Solution {
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//         int N = rooms.size();
+//         boolean[] visited = new boolean[N];
+//         Queue<Integer> q = new LinkedList<>();
+
+//         visited[0] = true;
+//         q.add(0);
+
+//         while (!q.isEmpty()) {
+//             int currRoom = q.poll();
+//             for (Integer room : rooms.get(currRoom)) {
+//                 if (!visited[room]) {
+//                     visited[room] = true;
+//                     q.add(room);
+//                 }
+//             }
+//         }
+
+//         boolean res = true;
+//         for (int i = 0; i < N; i++) {
+//             if (visited[i] == false) {
+//                 res = false;
+//                 break;
+//             }
+//         }
+
+//         return res;
+//     }
+// }
+
 class Solution {
+    int N;
+    boolean[] visited;
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int N = rooms.size();
-        boolean[] visited = new boolean[N];
-        Queue<Integer> q = new LinkedList<>();
+        N = rooms.size();
+        visited = new boolean[N];
 
-        visited[0] = true;
-        q.add(0);
-
-        while (!q.isEmpty()) {
-            int currRoom = q.poll();
-            for (Integer room : rooms.get(currRoom)) {
-                if (!visited[room]) {
-                    visited[room] = true;
-                    q.add(room);
-                }
-            }
-        }
+        dfs(rooms, 0);
 
         boolean res = true;
-        ;
         for (int i = 0; i < N; i++) {
             if (visited[i] == false) {
                 res = false;
@@ -38,6 +59,16 @@ class Solution {
         }
 
         return res;
+    }
+
+    public void dfs(List<List<Integer>> rooms, int currRoom) {
+        visited[currRoom] = true;
+        for (Integer room : rooms.get(currRoom)) {
+            if (!visited[room]) {
+                visited[room] = true;
+                dfs(rooms, room);
+            }
+        }
     }
 }
 // @lc code=end
