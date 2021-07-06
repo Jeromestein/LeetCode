857a28bf59ebf69e825e3b18a9f134157599052e
Jerome <nietsemorej@gmail.com>
2021-07-06 22:49:48 +0800

    Still confused about these iteration approaches.

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,69 +17,71 @@ import javax.swing.tree.TreeNode;
  * = left; this.right = right; } }
  */
 
-class Solution {
-    int n;
-    Map<Integer, Integer> indexMap;
-    int[] inorder, preorder;
-
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        n = inorder.length;
-        indexMap = new HashMap<>();
-        this.inorder = inorder;
-        this.preorder = preorder;
-
-        for (int i = 0; i < n; i++) {
-            indexMap.put(inorder[i], i);
-        }
-
-        return buildTreePreIn(0, n - 1, 0, n - 1);
-    }
-
-    public TreeNode buildTreePreIn(int preStart, int preEnd, int inStart, int inEnd) {
-        if (inStart > inEnd) {
-            return null;
-        }
-
-        int preRoot = preStart;
-        int inRoot = indexMap.get(preorder[preRoot]);
-
-        TreeNode root = new TreeNode(preorder[preRoot]);
-        int leftSubtreeSize = inRoot - inStart;
-
-        root.left = buildTreePreIn(preStart + 1, preStart + leftSubtreeSize, inStart, inRoot - 1);
-        root.right = buildTreePreIn(preStart + leftSubtreeSize + 1, preEnd, inRoot + 1, inEnd);
-        return root;
-    }
-}
-
 // class Solution {
+// int n;
+// Map<Integer, Integer> indexMap;
+// int[] inorder, preorder;
+
 // public TreeNode buildTree(int[] preorder, int[] inorder) {
-// if (preorder == null || preorder.length == 0) {
+// n = inorder.length;
+// indexMap = new HashMap<>();
+// this.inorder = inorder;
+// this.preorder = preorder;
+
+// for (int i = 0; i < n; i++) {
+// indexMap.put(inorder[i], i);
+// }
+
+// return buildTreePreIn(0, n - 1, 0, n - 1);
+// }
+
+// public TreeNode buildTreePreIn(int preStart, int preEnd, int inStart, int
+// inEnd) {
+// if (inStart > inEnd) {
 // return null;
 // }
-// TreeNode root = new TreeNode(preorder[0]);
-// Deque<TreeNode> stack = new LinkedList<TreeNode>();
-// stack.push(root);
 
-// int inorderIndex = 0;
-// for (int preorderIndex = 1; preorderIndex < preorder.length; preorderIndex++)
-// {
-// TreeNode node = stack.peek();
+// int preRoot = preStart;
+// int inRoot = indexMap.get(preorder[preRoot]);
 
-// if (node.val != inorder[inorderIndex]) {
-// node.left = new TreeNode(preorder[preorderIndex]);
-// stack.push(node.left);
-// } else {
-// while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
-// node = stack.pop();
-// inorderIndex++;
-// }
-// node.right = new TreeNode(preorder[preorderIndex]);
-// stack.push(node.right);
-// }
-// }
+// TreeNode root = new TreeNode(preorder[preRoot]);
+// int leftSubtreeSize = inRoot - inStart;
+
+// root.left = buildTreePreIn(preStart + 1, preStart + leftSubtreeSize, inStart,
+// inRoot - 1);
+// root.right = buildTreePreIn(preStart + leftSubtreeSize + 1, preEnd, inRoot +
+// 1, inEnd);
 // return root;
 // }
 // }
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+
+        int inorderIndex = 0;
+        for (int preorderIndex = 1; preorderIndex < preorder.length; preorderIndex++) {
+            TreeNode node = stack.peek();
+
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorder[preorderIndex]);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorder[preorderIndex]);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
 
 // @lc code=end
--- 106.construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -17,40 +17,70 @@ import javax.swing.tree.TreeNode;
  * = left; this.right = right; } }
  */
 
+// class Solution {
+// int n;
+// Map<Integer, Integer> indexMap;
+// int[] inorder, postorder;
+
+// public TreeNode buildTree(int[] inorder, int[] postorder) {
+// n = inorder.length;
+// indexMap = new HashMap<>();
+// this.inorder = inorder;
+// this.postorder = postorder;
+
+// for (int i = 0; i < n; i++) {
+// indexMap.put(inorder[i], i);
+// }
+
+// return buildTreeInPost(0, n - 1, 0, n - 1);
+// }
+
+// public TreeNode buildTreeInPost(int inStart, int inEnd, int postStart, int
+// postEnd) {
+// if (inStart > inEnd) {
+// return null;
+// }
+
+// int postRoot = postEnd;
+// int inRoot = indexMap.get(postorder[postRoot]);
+
+// TreeNode root = new TreeNode(postorder[postRoot]);
+// int rightSubtreeSize = inEnd - inRoot;
+
+// root.left = buildTreeInPost(inStart, inRoot - 1, postStart, postEnd -
+// rightSubtreeSize - 1);
+// root.right = buildTreeInPost(inRoot + 1, inEnd, postEnd - rightSubtreeSize,
+// postEnd - 1);
+
+// return root;
+// }
+// }
+
 class Solution {
-    int n;
-    Map<Integer, Integer> indexMap;
-    int[] inorder, postorder;
-
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        n = inorder.length;
-        indexMap = new HashMap<>();
-        this.inorder = inorder;
-        this.postorder = postorder;
-
-        for (int i = 0; i < n; i++) {
-            indexMap.put(inorder[i], i);
-        }
-
-        return buildTreeInPost(0, n - 1, 0, n - 1);
-    }
-
-    public TreeNode buildTreeInPost(int inStart, int inEnd, int postStart, int postEnd) {
-        if (inStart > inEnd) {
+        if (postorder == null || postorder.length == 0) {
             return null;
         }
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        int inorderIndex = inorder.length - 1;
+        for (int postorderIndex = postorder.length - 2; postorderIndex >= 0; postorderIndex--) {
+            TreeNode node = stack.peek();
 
-        int postRoot = postEnd;
-        int inRoot = indexMap.get(postorder[postRoot]);
-
-        TreeNode root = new TreeNode(postorder[postRoot]);
-        int rightSubtreeSize = inEnd - inRoot;
-
-        root.left = buildTreeInPost(inStart, inRoot - 1, postStart, postEnd - rightSubtreeSize - 1);
-        root.right = buildTreeInPost(inRoot + 1, inEnd, postEnd - rightSubtreeSize, postEnd - 1);
-
+            if (node.val != inorder[inorderIndex]) {
+                node.right = new TreeNode(postorder[postorderIndex]);
+                stack.push(node.right);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex--;
+                }
+                node.left = new TreeNode(postorder[postorderIndex]);
+                stack.push(node.left);
+            }
+        }
         return root;
     }
 }
-
 // @lc code=end
