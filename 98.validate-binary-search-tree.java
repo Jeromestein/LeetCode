0695edeb028c60d519618c5a53c88e6cf74b14b2
Jerome <nietsemorej@gmail.com>
2021-07-08 21:49:14 +0800

    iteration

--- 98.validate-binary-search-tree.java
+++ 98.validate-binary-search-tree.java
@@ -1,3 +1,7 @@
+import java.util.Deque;
+
+import javax.swing.tree.TreeNode;
+
 /*
  * @lc app=leetcode id=98 lang=java
  *
@@ -11,26 +15,48 @@
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
+// class Solution {
+// public boolean isValidBST(TreeNode root) {
+// return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+// }
+
+// // -2^31 <= Node.val <= 2^31 - 1
+// // so use long type rather than in
+// public boolean isValidBST(TreeNode root, long lowerBound, long upperBound) {
+// if (root == null)
+// return true;
+
+// // The left subtree of a node contains only nodes
+// // with keys less than the node's key.
+// // The right subtree of a node contains only nodes
+// // with keys greater than the node's key.
+// // so don't forget '='
+// if (root.val <= lowerBound || root.val >= upperBound)
+// return false;
+
+// return isValidBST(root.left, lowerBound, root.val) && isValidBST(root.right,
+// root.val, upperBound);
+// }
+// }
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        long preVal = Long.MIN_VALUE;
 
-    // -2^31 <= Node.val <= 2^31 - 1
-    // so use long type rather than in
-    public boolean isValidBST(TreeNode root, long lowerBound, long upperBound) {
-        if (root == null)
-            return true;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (preVal >= root.val)
+                return false;
+            preVal = root.val;
+            root = root.right;
+        }
 
-        // The left subtree of a node contains only nodes
-        // with keys less than the node's key.
-        // The right subtree of a node contains only nodes
-        // with keys greater than the node's key.
-        // so don't forget '='
-        if (root.val <= lowerBound || root.val >= upperBound)
-            return false;
-
-        return isValidBST(root.left, lowerBound, root.val) && isValidBST(root.right, root.val, upperBound);
+        return true;
     }
 }
 // @lc code=end
