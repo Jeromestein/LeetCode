e6b6c95ba1f39360bdccb7ccd28cd0d92152ef96
Jerome <nietsemorej@gmail.com>
2021-07-09 21:37:17 +0800

    0ms, 100%, 81%.

--- 110.balanced-binary-tree.java
+++ 110.balanced-binary-tree.java
@@ -11,22 +11,42 @@
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
+// class Solution {
+// public boolean isBalanced(TreeNode root) {
+// if (root == null)
+// return true;
+// int lHeight = getHeight(root.left);
+// int rHeight = getHeight(root.right);
+// int diff = Math.abs(lHeight - rHeight);
+
+// return diff <= 1 && isBalanced(root.left) && isBalanced(root.right);
+// }
+
+// public int getHeight(TreeNode root) {
+// if (root == null)
+// return 0;
+
+// return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+// }
+// }
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null)
-            return true;
-        int lHeight = getHeight(root.left);
-        int rHeight = getHeight(root.right);
-        int diff = Math.abs(lHeight - rHeight);
-
-        return diff <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        return getHeight(root) != -1;
     }
 
     public int getHeight(TreeNode root) {
         if (root == null)
             return 0;
-
-        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+        int lHeight = getHeight(root.left);
+        int rHeight = getHeight(root.right);
+        int diff = Math.abs(lHeight - rHeight);
+        if (lHeight == -1 || rHeight == -1 || diff > 1) {
+            return -1;
+        } else {
+            return Math.max(lHeight, rHeight) + 1;
+        }
     }
 }
+
 // @lc code=end
