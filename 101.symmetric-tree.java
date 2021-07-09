4a749a708dc623da43f79eab5672a604bd28b2ae
Jerome <nietsemorej@gmail.com>
2021-07-09 21:07:49 +0800

    iteration

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -11,21 +11,55 @@
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
+// class Solution {
+// public boolean isSymmetric(TreeNode root) {
+// return isSymmetric(root.left, root.right);
+// }
+
+// // if two tree start from lRoot and rRoot is Symmetric
+// public boolean isSymmetric(TreeNode lRoot, TreeNode rRoot) {
+// if (lRoot == null && rRoot == null)
+// return true;
+// if (lRoot == null ^ rRoot == null)
+// return false;
+// if (lRoot.val != rRoot.val)
+// return false;
+
+// return isSymmetric(lRoot.left, rRoot.right) && isSymmetric(lRoot.right,
+// rRoot.left);
+// }
+// }
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return isSymmetric(root.left, root.right);
-    }
+        Queue<TreeNode> lQueue = new LinkedList<>();
+        Queue<TreeNode> rQueue = new LinkedList<>();
+        lQueue.add(root);
+        rQueue.add(root);
+        while (!lQueue.isEmpty() && !rQueue.isEmpty()) {
+            TreeNode lNode = lQueue.poll();
+            TreeNode rNode = rQueue.poll();
+            if (lNode.val != rNode.val)
+                return false;
 
-    // if two tree start from lRoot and rRoot is Symmetric
-    public boolean isSymmetric(TreeNode lRoot, TreeNode rRoot) {
-        if (lRoot == null && rRoot == null)
-            return true;
-        if (lRoot == null ^ rRoot == null)
-            return false;
-        if (lRoot.val != rRoot.val)
-            return false;
-
-        return isSymmetric(lRoot.left, rRoot.right) && isSymmetric(lRoot.right, rRoot.left);
+            if (lNode.left == null ^ rNode.right == null) {
+                // node1.left != null && node2.left == null ||
+                // node1.left == null && node2.left != null
+                return false;
+            } else if (lNode.left != null && rNode.right != null) {
+                lQueue.add(lNode.left);
+                rQueue.add(rNode.right);
+            }
+            if (lNode.right == null ^ rNode.left == null) {
+                // node1.right != null && node2.right == null ||
+                // node1.right == null && node2.right != null
+                return false;
+            } else if (lNode.right != null && rNode.left != null) {
+                lQueue.add(lNode.right);
+                rQueue.add(rNode.left);
+            }
+        }
+        return lQueue.isEmpty() && rQueue.isEmpty();
     }
 }
 // @lc code=end
