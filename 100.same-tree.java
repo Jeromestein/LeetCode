0eb830216b6b89817032ef7c21eecf264767a099
Jerome <nietsemorej@gmail.com>
2021-07-09 20:52:51 +0800

    recursion

--- 100.same-tree.java
+++ 100.same-tree.java
@@ -18,38 +18,61 @@ import javax.swing.tree.TreeNode;
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
+// class Solution {
+// public boolean isSameTree(TreeNode p, TreeNode q) {
+// if (p == null && q == null)
+// return true;
+// if (p == null ^ q == null)
+// // ^: xor
+// // p != null && q == null || p == null && q != null
+// return false;
+
+// Queue<TreeNode> pQueue = new LinkedList<>();
+// Queue<TreeNode> qQueue = new LinkedList<>();
+// pQueue.add(p);
+// qQueue.add(q);
+// while (!pQueue.isEmpty() && !qQueue.isEmpty()) {
+// TreeNode node1 = pQueue.poll();
+// TreeNode node2 = qQueue.poll();
+// if (node1.val != node2.val)
+// return false;
+
+// if (node1.left == null ^ node2.left == null) {
+// // node1.left != null && node2.left == null ||
+// // node1.left == null && node2.left != null
+// return false;
+// } else if (node1.left != null && node2.left != null) {
+// pQueue.add(node1.left);
+// qQueue.add(node2.left);
+// }
+
+// if (node1.right == null ^ node2.right == null) {
+// // node1.right != null && node2.right == null ||
+// // node1.right == null && node2.right != null
+// return false;
+// } else if (node1.right != null && node2.right != null) {
+// pQueue.add(node1.right);
+// qQueue.add(node2.right);
+// }
+// }
+
+// return pQueue.isEmpty() && qQueue.isEmpty();
+// }
+// }
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null)
             return true;
         if (p == null ^ q == null)
+            // ^: xor
+            // p != null && q == null || p == null && q != null
             return false;
 
-        Queue<TreeNode> pQueue = new LinkedList<>();
-        Queue<TreeNode> qQueue = new LinkedList<>();
-        pQueue.add(p);
-        qQueue.add(q);
-        while (!pQueue.isEmpty() && !qQueue.isEmpty()) {
-            TreeNode node1 = pQueue.poll();
-            TreeNode node2 = qQueue.poll();
-            if (node1.val != node2.val)
-                return false;
-            if (node1.left == null ^ node2.left == null) {
-                return false;
-            } else if (node1.left != null && node2.left != null) {
-                pQueue.add(node1.left);
-                qQueue.add(node2.left);
-            }
-            if (node1.right == null ^ node2.right == null) {
-                return false;
-            } else if (node1.right != null && node2.right != null) {
-                pQueue.add(node1.right);
-                qQueue.add(node2.right);
-            }
+        if (p.val != q.val)
+            return false;
 
-        }
-
-        return pQueue.isEmpty() && qQueue.isEmpty();
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
 // @lc code=end
