3e808e5bd1b1fda50b1af8be50233937ef1c63d6
Jerome <nietsemorej@gmail.com>
2021-07-09 21:31:42 +0800

    1ms, 51%, 12%. recursion.

---
+++ 110.balanced-binary-tree.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        int lHeight = getHeight(root.left);
+        int rHeight = getHeight(root.right);
+        int diff = Math.abs(lHeight - rHeight);
+
+        return diff <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+}
+// @lc code=end
