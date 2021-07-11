e4530cf79a8af1a85f8ce2b86e0cb0ef5a634132
Jerome <nietsemorej@gmail.com>
2021-07-11 20:34:01 +0800

    faster

--- 331.verify-preorder-serialization-of-a-binary-tree.java
+++ 331.verify-preorder-serialization-of-a-binary-tree.java
@@ -8,20 +8,25 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
         int nodeCNT = 0, nullCNT = 0;
-        String[] inputs = preorder.split(",");
 
-        for (String in : inputs) {
+        for (int i = 0; i < preorder.length(); i++) {
             if (nullCNT > nodeCNT) {
                 // for any binary tree, nullCNT=nodeCNT+1
                 return false;
             }
 
-            if (in.equals("#")) {
-                nullCNT++;
-            } else {
-                nodeCNT++;
+            if (preorder.charAt(i) != ',') {
+                if (preorder.charAt(i) == '#') {
+                    // receive a null
+                    nullCNT++;
+                } else {
+                    // receive a node
+                    nodeCNT++;
+                    while (i < preorder.length() && preorder.charAt(i) != ',') {
+                        i++;
+                    }
+                }
             }
-
         }
 
         if (nullCNT != nodeCNT + 1) {
