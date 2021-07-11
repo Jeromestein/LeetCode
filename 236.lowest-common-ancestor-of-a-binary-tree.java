b2c93ae326ae6eec454b5edec98568ff69614c68
Jerome <nietsemorej@gmail.com>
2021-07-11 19:55:02 +0800

    5ms,  41%, 63%. recursion.

--- 236.lowest-common-ancestor-of-a-binary-tree.java
+++ 236.lowest-common-ancestor-of-a-binary-tree.java
@@ -17,77 +17,28 @@ import javax.swing.tree.TreeNode;
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 class Solution {
-    // adjacent list <child, father>
-    Map<TreeNode, TreeNode> adjList = new HashMap<>();
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        // bfs starting from root, create adjacent list and find p and q
-        queue.add(root);
+        if (root == p || root == q)
+            return root;
 
-        boolean isFound_p = false;
-        boolean isFound_q = false;
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node == p) {
-                isFound_p = true;
-            }
-            if (node == q) {
-                isFound_q = true;
-            }
+        if (root == null)
+            return null;
 
-            if (isFound_p == true && isFound_q == true) {
-                break;
-            }
+        TreeNode lNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rNode = lowestCommonAncestor(root.right, p, q);
 
-            if (node.left != null) {
-                // create adjacent list
-                adjList.put(node.left, node);
-                queue.add(node.left);
-            }
-            if (node.right != null) {
-                // create adjacent list
-                adjList.put(node.right, node);
-                queue.add(node.right);
-            }
+        if (lNode != null && rNode != null) {
+            // one in left subtree, another in right subtree
+            return root;
+        } else if (lNode == null) {
+            // left sbutree is empty, both of two maybe in right subtree
+            return rNode;
+        } else {
+            // left sbutree is not empty and right sbutree is empty,
+            // both of two maybe in left subtree
+            return lNode;
         }
 
-        return getLCA(root, p, q);
-    }
-
-    public TreeNode getLCA(TreeNode root, TreeNode p, TreeNode q) {
-        // get the length of linklist of p and q (from p/q to root)
-        int pLen = 1, qLen = 1;
-        TreeNode node = p;
-        while (node != root) {
-            node = adjList.get(node);
-            pLen++;
-        }
-        node = q;
-        while (node != root) {
-            node = adjList.get(node);
-            qLen++;
-        }
-
-        if (pLen > qLen) {
-            while (pLen != qLen) {
-                p = adjList.get(p);
-                pLen--;
-            }
-        }
-        if (pLen < qLen) {
-            while (pLen != qLen) {
-                q = adjList.get(q);
-                qLen--;
-            }
-        }
-
-        while (p != q) {
-            p = adjList.get(p);
-            q = adjList.get(q);
-        }
-
-        return p;
     }
 }
 // @lc code=end
