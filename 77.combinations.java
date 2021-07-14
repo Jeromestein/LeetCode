099c773ddb41f4acc40b43d3e603ad92ba0d8ca4
Jerome <nietsemorej@gmail.com>
2021-07-14 17:12:44 +0800

    Add comments

--- 77.combinations.java
+++ 77.combinations.java
@@ -19,6 +19,7 @@ class Solution {
         if (k == 0)
             res.add(new ArrayList<>(tempList));
         else {
+            // prune: i + k - 1 <= n
             for (int i = start; i + k - 1 <= n; i++) {
                 tempList.add(i);
                 backtrack(res, tempList, n, k - 1, i + 1);
