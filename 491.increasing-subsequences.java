c93f5f6efe7f3a0b51781259c60afddfe28f2943
Jerome <nietsemorej@gmail.com>
2021-07-16 21:33:34 +0800

    new template, 2ms, 100%, 95%.

--- 491.increasing-subsequences.java
+++ 491.increasing-subsequences.java
@@ -11,30 +11,59 @@ import java.util.Set;
  */
 
 // @lc code=start
-class Solution {
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        // don't store duplicates
-        Set<List<Integer>> res = new HashSet<>();
+// class Solution {
+//     public List<List<Integer>> findSubsequences(int[] nums) {
+//         // don't store duplicates
+//         Set<List<Integer>> res = new HashSet<>();
 
-        // Arrays.sort(nums); // don't sort nums[] !!!
-        backtrack(nums, res, new ArrayList<>(), 0);
-        return new ArrayList<>(res);
+//         // Arrays.sort(nums); // don't sort nums[] !!!
+//         backtrack(nums, res, new ArrayList<>(), 0);
+//         return new ArrayList<>(res);
+//     }
+
+//     public void backtrack(int[] nums, Set<List<Integer>> res, List<Integer> temp, int idx) {
+//         if (temp.size() > 1) {
+//             res.add(new ArrayList<>(temp));
+//         }
+
+//         for (int i = idx; i < nums.length; i++) {
+//             if (temp.isEmpty() || nums[i] >= temp.get(temp.size() - 1)) {
+//                 // if temp is empty or nums[i] >= the last element of temp
+//                 temp.add(nums[i]);
+//                 backtrack(nums, res, temp, i + 1);
+//                 temp.remove(temp.size() - 1);
+//             }
+//         }
+//     }
+// }
+
+class Solution {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        dfs(0, Integer.MIN_VALUE, nums);
+        return ans;
     }
 
-    public void backtrack(int[] nums, Set<List<Integer>> res, List<Integer> temp, int idx) {
-        if (temp.size() > 1) {
-            int last = temp.get(temp.size() - 1), secLast = temp.get(temp.size() - 2);
-            if (secLast > last) {
-                return;
-            } else {
-                res.add(new ArrayList<>(temp));
+    public void dfs(int cur, int last, int[] nums) {
+        if (cur == nums.length) {
+            if (temp.size() >= 2) {
+                ans.add(new ArrayList<Integer>(temp));
             }
+            return;
         }
 
-        for (int i = idx; i < nums.length; i++) {
-            temp.add(nums[i]);
-            backtrack(nums, res, temp, i + 1);
+        if (nums[cur] >= last) {
+            // choose current element
+            temp.add(nums[cur]);
+            dfs(cur + 1, nums[cur], nums);
             temp.remove(temp.size() - 1);
+        }
+
+        if (nums[cur] != last) {
+            // not choose current element
+            dfs(cur + 1, last, nums);
         }
     }
 }
