f66a9525e042943bfd4757e1c38ee82dab1dfbfb
Jeromestein <nietsemorej@gmail.com>
2021-07-17 22:55:40 +0800

    Add comments

--- 37.sudoku-solver.java
+++ 37.sudoku-solver.java
@@ -15,6 +15,7 @@ class Solution {
     private boolean[][] column = new boolean[9][9];
     private boolean[][][] block = new boolean[3][3][9];
     private boolean valid = false;
+    // the space of empty units
     private List<int[]> spaces = new ArrayList<int[]>();
 
     public void solveSudoku(char[][] board) {
@@ -34,19 +35,20 @@ class Solution {
         dfs(board, 0);
     }
 
-    public void dfs(char[][] board, int pos) {
-        if (pos == spaces.size()) {
+    // cnt is the no. of filled units which were empty
+    public void dfs(char[][] board, int cnt) {
+        if (cnt == spaces.size()) {
             valid = true;
             return;
         }
 
-        int[] space = spaces.get(pos);
+        int[] space = spaces.get(cnt);
         int i = space[0], j = space[1];
         for (int digit = 0; digit < 9 && !valid; ++digit) {
             if (!row[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit]) {
                 row[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
                 board[i][j] = (char) (digit + '0' + 1);
-                dfs(board, pos + 1);
+                dfs(board, cnt + 1);
                 row[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
             }
         }
