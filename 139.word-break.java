3f2eb433f8e27033d4610b7689a53744586ccc93
Jerome <nietsemorej@gmail.com>
2021-07-20 17:02:49 +0800

    1ms, 99%, 97%. pruning.

--- 139.word-break.java
+++ 139.word-break.java
@@ -16,8 +16,16 @@ class Solution {
         // dp[0] means empty string, valid
         dp[0] = true;
 
+        // get max word length
+        int maxWordLen = 0;
+        for (String word : wordDict) {
+            maxWordLen = Math.max(maxWordLen, word.length());
+        }
+
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
+            // don't need to walk though all the s[0,i-1]
+            // if even the longest word can't fit in, dp[i] should be false
+            for (int j = i; j >= 0 && j >= i - maxWordLen; j--) {
                 if (dp[j] && wordDictSet.contains(s.subSequence(j, i))) {
                     dp[i] = true;
                     break;
