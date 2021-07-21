8e03a614f7efc68e4106994a1ac2c4a8427684d2
Jerome <nietsemorej@gmail.com>
2021-07-21 17:49:57 +0800

    my code, 0ms, 100%, 62%.

---
+++ 48.rotate-image.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[][] matrix) {
+        // You are given an n x n 2D matrix
+        int n = matrix.length;
+        for (int i = 0; i < (n) / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
+// @lc code=end
