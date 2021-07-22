3648ef5262fc97a5017f10022f43478840bf2572
Jerome <nietsemorej@gmail.com>
2021-07-22 18:36:43 +0800

    Add comments

--- 6.zig-zag-conversion.java
+++ 6.zig-zag-conversion.java
@@ -12,12 +12,16 @@ class Solution {
 
         StringBuilder ret = new StringBuilder();
         int n = s.length();
+        // numRows+numRows-2
         int cycleLen = 2 * numRows - 2;
 
         for (int i = 0; i < numRows; i++) {
+            // traverse in level/row order
             for (int j = 0; j + i < n; j += cycleLen) {
                 ret.append(s.charAt(j + i));
                 if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    // if it is not the first row or last row
+                    // we should append the element in diagonals
                     ret.append(s.charAt(j + cycleLen - i));
             }
         }
