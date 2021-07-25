afb6c8ac923cbb2ec998b4fc674ebe85e7d2f941
Jerome <nietsemorej@gmail.com>
2021-07-25 19:38:22 +0800

    one pass

--- 92.reverse-linked-list-ii.java
+++ 92.reverse-linked-list-ii.java
@@ -10,43 +10,24 @@
  * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
  * ListNode next) { this.val = val; this.next = next; } }
  */
+
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        // 1. get pre head
-        ListNode preLeft = new ListNode(0, head);
-        ListNode oldHead = head, oldTail = head;
-        int i = 1;
-        while (i < left) {
-            preLeft = oldHead;
-
-            oldHead = oldHead.next;
-            oldTail = oldHead;
-            i++;
+        ListNode preHead = new ListNode(0, head);
+        ListNode pre = preHead;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
         }
-        while (i < right) {
-            oldTail = oldTail.next;
-            i++;
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
         }
-
-        preLeft.next = myReverseLinkedList(oldHead, oldTail)[0];
-
-        return left == 1 ? oldTail : head;
-    }
-
-    public ListNode[] myReverseLinkedList(ListNode oldHead, ListNode oldTail) {
-        ListNode oldTailNext = oldTail.next;
-        ListNode first = oldHead;
-
-        while (oldTailNext != oldTail) {
-            ListNode firstNext = first.next;
-
-            first.next = oldTailNext;
-            oldTailNext = first;
-
-            first = firstNext;
-        }
-
-        return new ListNode[] { oldTail, oldHead };
+        return preHead.next;
     }
 }
+
 // @lc code=end
