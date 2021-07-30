38c477bb0f2e49dc21a993b6af42aa39c4cf6f5b
Jerome <nietsemorej@gmail.com>
2021-07-30 22:23:50 +0800

    Add comments

--- 793.preimage-size-of-factorial-zeroes-function.java
+++ 793.preimage-size-of-factorial-zeroes-function.java
@@ -10,14 +10,14 @@ class Solution {
     public int preimageSizeFZF(long K) {
         long lo = K, hi = 10 * K + 1;
         while (lo < hi) {
-            long mi = lo + (hi - lo) / 2;
-            long zmi = zeta(mi);
-            if (zmi == K)
+            long mid = lo + (hi - lo) / 2;
+            long zmid = zeta(mid);
+            if (zmid == K)
                 return 5;
-            else if (zmi < K)
-                lo = mi + 1;
+            else if (zmid < K)
+                lo = mid + 1;
             else
-                hi = mi;
+                hi = mid;
         }
         return 0;
     }
