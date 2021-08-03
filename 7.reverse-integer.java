85e01259910373410312bd7558c21cff2d738333
Jerome <nietsemorej@gmail.com>
2021-08-03 23:07:35 +0800

    Add comments

--- 7.reverse-integer.java
+++ 7.reverse-integer.java
@@ -8,28 +8,22 @@
 class Solution {
     public int reverse(int x) {
         int reverseNum = 0;
-        int sign = 1;
         // Assume the environment
         // does not allow you to store 64-bit integers (signed or unsigned).
         // -2^31 <= x <= 2^31 - 1
         // -2147483648<= x <= 2147483647
-        if (x <= Integer.MIN_VALUE + 6 || x >= Integer.MAX_VALUE - 5) {
-            return 0;
-        }
-        if (x < 0) {
-            x *= -1;
-            sign = -1;
-        }
+
         while (x != 0) {
-            reverseNum *= 10;
-            reverseNum += x % 10;
-            x /= 10;
-            if (x != 0 && reverseNum > Integer.MAX_VALUE / 10) {
+            if (reverseNum > Integer.MAX_VALUE / 10 || reverseNum < Integer.MIN_VALUE / 10) {
                 return 0;
             }
+            int currDigit = x % 10;
+            reverseNum = reverseNum * 10 + currDigit;
+
+            x /= 10;
         }
 
-        return reverseNum * sign;
+        return reverseNum;
     }
 }
 // @lc code=end
