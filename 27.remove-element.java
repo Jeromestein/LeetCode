ac5469465b93661d718272ab35dc7f597beb847b
Jerome <nietsemorej@gmail.com>
2021-08-05 23:50:37 +0800

    0ms ,100%, 25%.

---
+++ 27.remove-element.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=27 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+}
+// @lc code=end
