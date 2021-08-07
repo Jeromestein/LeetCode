31147bc53b09d1f20fe507e5c93e79c0e17442a7
Jerome <nietsemorej@gmail.com>
2021-08-07 23:27:13 +0800

    my code, 0ms, 100%, 6%.

---
+++ 35.search-insert-position.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo < hi) {
+            // 1 <= nums.length <= 10^4
+            int mid = (lo + hi) / 2;
+            if (target > nums[mid]) {
+                lo = mid + 1;
+            } else if (target < nums[mid]) {
+                hi = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        if (nums[lo] >= target) {
+            return lo;
+        } else {
+            return lo + 1;
+        }
+    }
+}
+// @lc code=end
