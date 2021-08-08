482d0fa413b8e3cdcca56a4c6db511bf9e8b5a4c
Jerome <nietsemorej@gmail.com>
2021-08-08 21:12:43 +0800

    0ms, 100%, 94%.

--- 58.length-of-last-word.java
+++ 58.length-of-last-word.java
@@ -7,8 +7,21 @@
 // @lc code=start
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
-        return words[words.length - 1].length();
+        // String[] words = s.split(" ");
+        // return words[words.length - 1].length();
+        int start = 0, end = s.length() - 1;
+
+        // s consists of only English letters and spaces ' '.
+
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+
+        return end - start;
     }
 }
 // @lc code=end
