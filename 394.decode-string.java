5d0ebc963345da8bfb8ea97effa9196334a0a187
Jerome <nietsemorej@gmail.com>
2021-08-24 08:29:35 -0400

    Add comments

--- 394.decode-string.java
+++ 394.decode-string.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.Deque;
+
 /*
  * @lc app=leetcode id=394 lang=java
  *
@@ -10,36 +13,43 @@ class Solution {
     int ptr;
 
     public String decodeString(String s) {
-        LinkedList<String> stk = new LinkedList<String>();
+        Deque<String> stk = new LinkedList<String>();
         ptr = 0;
 
         while (ptr < s.length()) {
-            char cur = s.charAt(ptr);
-            if (Character.isDigit(cur)) {
-                // 获取一个数字并进栈
-                String digits = getDigits(s);
-                stk.addLast(digits);
-            } else if (Character.isLetter(cur) || cur == '[') {
-                // 获取一个字母并进栈
-                stk.addLast(String.valueOf(s.charAt(ptr++)));
-            } else {
-                ++ptr;
-                LinkedList<String> sub = new LinkedList<String>();
+            char currDigit = s.charAt(ptr);
+            // s consists of lowercase English letters, digits, and square brackets '[' &
+            // ']'.
+            if (Character.isDigit(currDigit)) {
+                // get multi digits number in the type of string
+                String num = getNum(s);
+                stk.addLast(num);
+            } else if (Character.isLetter(currDigit) || currDigit == '[') {
+                // get a letter in the type of string
+                stk.addLast(String.valueOf(s.charAt(ptr)));
+                ptr++;
+            } else if (currDigit == ']') {
+                // skip ']'
+                ptr++;
+
+                // get the encoded_string between '[' and ']'
+                Deque<String> sub = new LinkedList<String>();
                 while (!"[".equals(stk.peekLast())) {
-                    sub.addLast(stk.removeLast());
+                    sub.addFirst(stk.removeLast());
                 }
-                Collections.reverse(sub);
-                // 左括号出栈
+
+                // pop the '['
                 stk.removeLast();
-                // 此时栈顶为当前 sub 对应的字符串应该出现的次数
-                int repTime = Integer.parseInt(stk.removeLast());
-                StringBuffer t = new StringBuffer();
-                String o = getString(sub);
-                // 构造字符串
-                while (repTime-- > 0) {
-                    t.append(o);
+
+                int k = Integer.parseInt(stk.removeLast());
+                StringBuilder t = new StringBuilder();
+                String encoded_string = getString(sub);
+
+                // repeat the encoded_string based on k
+                while (k-- > 0) {
+                    t.append(encoded_string);
                 }
-                // 将构造好的字符串入栈
+
                 stk.addLast(t.toString());
             }
         }
@@ -47,20 +57,22 @@ class Solution {
         return getString(stk);
     }
 
-    public String getDigits(String s) {
-        StringBuffer ret = new StringBuffer();
+    // get multi digits number
+    public String getNum(String s) {
+        StringBuilder res = new StringBuilder();
         while (Character.isDigit(s.charAt(ptr))) {
-            ret.append(s.charAt(ptr++));
+            res.append(s.charAt(ptr));
+            ptr++;
         }
-        return ret.toString();
+        return res.toString();
     }
 
-    public String getString(LinkedList<String> v) {
-        StringBuffer ret = new StringBuffer();
+    public String getString(Deque<String> v) {
+        StringBuilder res = new StringBuilder();
         for (String s : v) {
-            ret.append(s);
+            res.append(s);
         }
-        return ret.toString();
+        return res.toString();
     }
 }
 
