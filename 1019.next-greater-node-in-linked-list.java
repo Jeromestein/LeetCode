526b3be7f0ed56c999450d683ba1f879cd40a7fd
Jerome <nietsemorej@gmail.com>
2021-08-26 08:21:36 -0400

    8ms, 98%, 16%.

--- 1019.next-greater-node-in-linked-list.java
+++ 1019.next-greater-node-in-linked-list.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,39 +16,40 @@ import java.util.List;
  * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
  * ListNode next) { this.val = val; this.next = next; } }
  */
+
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        ListNode tail = reverse(head);
-        Deque<Integer> stack = new LinkedList<>();
-        int[] res = new int[tail.val];
-        int index = tail.val - 1;
-        tail = tail.next;
-        while (index >= 0) {
-            while (!stack.isEmpty() && tail.val >= stack.peek()) {
-                stack.poll();
-            }
-            res[index--] = stack.isEmpty() ? 0 : stack.peek();
-            stack.push(tail.val);
-            tail = tail.next;
-        }
-        return res;
-    }
+        int n = 0;
+        ListNode temp = head;
 
-    private ListNode reverse(ListNode head) {
-        // 之所以返回dummy是为了用dummy.val保存链表的长度
-        ListNode dummy = new ListNode(-1);
-        int count = 0;
-        ListNode cur = head, pre = null;
-        while (cur != null) {
-            ListNode temp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = temp;
-            count++;
+        while (temp != null) {
+            temp = temp.next;
+            n++;
         }
-        dummy.val = count;
-        dummy.next = pre;
-        return dummy;
+
+        // to store the values of the nodes
+        int[] nums = new int[n];
+        // to store next greater element of each node (default 0)
+        int[] res = new int[n];
+        Arrays.fill(res, 0);
+        // to store the indexs
+        Deque<Integer> stk = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            // get the current value and store it in array
+            int currVal = head.val;
+            nums[i] = currVal;
+
+            // for all the recently seen elements smaller than curr
+            while (!stk.isEmpty() && currVal > nums[stk.peek()]) {
+                res[stk.pop()] = currVal;
+            }
+
+            stk.push(i);
+            head = head.next;
+        }
+
+        return res;
     }
 }
 // @lc code=end
