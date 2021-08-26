f61a1f68c2dee1e60549847cd711752b5f35b3f6
Jerome <nietsemorej@gmail.com>
2021-08-26 09:31:19 -0400

    58ms, 13%, 12%.

--- 208.implement-trie-prefix-tree.java
+++ 208.implement-trie-prefix-tree.java
@@ -8,36 +8,55 @@ import java.util.Set;
  */
 
 // @lc code=start
+
 class Trie {
-    Set<String> set;
+    private Trie[] children;
+    private boolean isEnd;
 
     /** Initialize your data structure here. */
     public Trie() {
-        set = new HashSet<>();
-
+        children = new Trie[26];
+        isEnd = false;
     }
 
     /** Inserts a word into the trie. */
     public void insert(String word) {
-        set.add(word);
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
     }
 
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        return set.contains(word);
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
     }
 
     /**
      * Returns if there is any word in the trie that starts with the given prefix.
      */
     public boolean startsWith(String prefix) {
-        for (String string : set) {
-            if (string.indexOf(prefix) == 0) {
-                return true;
-            }
-        }
+        return searchPrefix(prefix) != null;
+    }
 
-        return false;
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
     }
 }
 
