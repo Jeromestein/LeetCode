0b5075df776f56a3eb1b0a7f4c6e588a1106490e
Jerome <nietsemorej@gmail.com>
2021-08-27 11:53:03 -0400

    segment tree: add comments

--- 307.range-sum-query-mutable.java
+++ 307.range-sum-query-mutable.java
@@ -19,8 +19,10 @@ class NumArray {
     }
 
     private void buildTree(int[] nums) {
-        for (int i = n, j = 0; i < 2 * n; i++, j++)
-            tree[i] = nums[j];
+        for (int i = 0; i < n; i++) {
+            tree[i + n] = nums[i];
+        }
+
         for (int i = n - 1; i > 0; --i)
             tree[i] = tree[i * 2] + tree[i * 2 + 1];
     }
@@ -28,7 +30,10 @@ class NumArray {
     // Updates the value of nums[index] to be val.
     public void update(int index, int val) {
         index += n;
+
+        // update num itself
         tree[index] = val;
+        // update related sums
         while (index > 0) {
             int left = index;
             int right = index;
@@ -44,8 +49,8 @@ class NumArray {
 
     }
 
-    // Returns the sum of the elements of nums between indices left and right
-    // inclusive
+    // Returns the sum of the elements of nums
+    // between indices left and right inclusive
     // (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
     public int sumRange(int left, int right) {
         // get leaf with value 'l'
@@ -69,6 +74,42 @@ class NumArray {
 
     }
 }
+
+// class NumArray {
+// int t=0;
+// int[] a;
+// public NumArray(int[] nums) {
+// a=nums;
+// for(int i:a)
+// t+=i;
+// }
+
+// public void update(int index, int val) {
+// t-=a[index];
+// t+=val;
+// a[index]=val;
+// }
+
+// public int sumRange(int left, int right) {
+// int s=0;
+// int size=right-left+1;
+// if(Math.abs(a.length-size)>size)
+// {
+// for(int i=left;i<=right;i++)
+// s+=a[i];
+
+// return s;
+// }
+// else
+// {
+// for(int i=0;i<left;i++)
+// s+=a[i];
+// for(int i=right+1;i<a.length;i++)
+// s+=a[i];
+// }
+// return t-s;
+// }
+// }
 
 /**
  * Your NumArray object will be instantiated and called as such: NumArray obj =
