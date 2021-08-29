8013b0a207eb4d3ccfaaf16097a3a9731d26bd04
Jerome <nietsemorej@gmail.com>
2021-08-29 09:22:46 -0400

    44ms, 99%, 87%.

--- 460.lfu-cache.java
+++ 460.lfu-cache.java
@@ -7,95 +7,111 @@
 // @lc code=start
 
 class LFUCache {
-    // 缓存容量，时间戳
-    int capacity, time;
-    Map<Integer, Node> key_table;
-    TreeSet<Node> S;
+    Map<Integer, Node> cache; // 存储缓存的内容
+    Map<Integer, DoublyLinkedList> freqMap; // 存储每个频次对应的双向链表
+    int size;
+    int capacity;
+    int min; // 存储当前最小频次
 
     public LFUCache(int capacity) {
+        cache = new HashMap<>(capacity);
+        freqMap = new HashMap<>();
         this.capacity = capacity;
-        this.time = 0;
-        key_table = new HashMap<Integer, Node>();
-        S = new TreeSet<Node>();
     }
 
     public int get(int key) {
-        if (capacity == 0) {
+        Node node = cache.get(key);
+        if (node == null) {
             return -1;
         }
-        // 如果哈希表中没有键 key，返回 -1
-        if (!key_table.containsKey(key)) {
-            return -1;
-        }
-        // 从哈希表中得到旧的缓存
-        Node cache = key_table.get(key);
-        // 从平衡二叉树中删除旧的缓存
-        S.remove(cache);
-        // 将旧缓存更新
-        cache.cnt += 1;
-        cache.time = ++time;
-        // 将新缓存重新放入哈希表和平衡二叉树中
-        S.add(cache);
-        key_table.put(key, cache);
-        return cache.value;
+        freqInc(node);
+        return node.value;
     }
 
     public void put(int key, int value) {
         if (capacity == 0) {
             return;
         }
-        if (!key_table.containsKey(key)) {
-            // 如果到达缓存容量上限
-            if (key_table.size() == capacity) {
-                // 从哈希表和平衡二叉树中删除最近最少使用的缓存
-                key_table.remove(S.first().key);
-                S.remove(S.first());
-            }
-            // 创建新的缓存
-            Node cache = new Node(1, ++time, key, value);
-            // 将新缓存放入哈希表和平衡二叉树中
-            key_table.put(key, cache);
-            S.add(cache);
+        Node node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            freqInc(node);
         } else {
-            // 这里和 get() 函数类似
-            Node cache = key_table.get(key);
-            S.remove(cache);
-            cache.cnt += 1;
-            cache.time = ++time;
-            cache.value = value;
-            S.add(cache);
-            key_table.put(key, cache);
+            if (size == capacity) {
+                DoublyLinkedList minFreqLinkedList = freqMap.get(min);
+                cache.remove(minFreqLinkedList.tail.pre.key);
+                minFreqLinkedList.removeNode(minFreqLinkedList.tail.pre); // 这里不需要维护min, 因为下面add了newNode后min肯定是1.
+                size--;
+            }
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            DoublyLinkedList linkedList = freqMap.get(1);
+            if (linkedList == null) {
+                linkedList = new DoublyLinkedList();
+                freqMap.put(1, linkedList);
+            }
+            linkedList.addNode(newNode);
+            size++;
+            min = 1;
         }
+    }
+
+    void freqInc(Node node) {
+        // 从原freq对应的链表里移除, 并更新min
+        int freq = node.freq;
+        DoublyLinkedList linkedList = freqMap.get(freq);
+        linkedList.removeNode(node);
+        if (freq == min && linkedList.head.post == linkedList.tail) {
+            min = freq + 1;
+        }
+        // 加入新freq对应的链表
+        node.freq++;
+        linkedList = freqMap.get(freq + 1);
+        if (linkedList == null) {
+            linkedList = new DoublyLinkedList();
+            freqMap.put(freq + 1, linkedList);
+        }
+        linkedList.addNode(node);
     }
 }
 
-class Node implements Comparable<Node> {
-    int cnt, time, key, value;
+class Node {
+    int key;
+    int value;
+    int freq = 1;
+    Node pre;
+    Node post;
 
-    Node(int cnt, int time, int key, int value) {
-        this.cnt = cnt;
-        this.time = time;
+    public Node() {
+    }
+
+    public Node(int key, int value) {
         this.key = key;
         this.value = value;
     }
+}
 
-    public boolean equals(Object anObject) {
-        if (this == anObject) {
-            return true;
-        }
-        if (anObject instanceof Node) {
-            Node rhs = (Node) anObject;
-            return this.cnt == rhs.cnt && this.time == rhs.time;
-        }
-        return false;
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList() {
+        head = new Node();
+        tail = new Node();
+        head.post = tail;
+        tail.pre = head;
     }
 
-    public int compareTo(Node rhs) {
-        return cnt == rhs.cnt ? time - rhs.time : cnt - rhs.cnt;
+    void removeNode(Node node) {
+        node.pre.post = node.post;
+        node.post.pre = node.pre;
     }
 
-    public int hashCode() {
-        return cnt * 1000000007 + time;
+    void addNode(Node node) {
+        node.post = head.post;
+        head.post.pre = node;
+        head.post = node;
+        node.pre = head;
     }
 }
 
