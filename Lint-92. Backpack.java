353b58931477998fcea33081a97a1e02b21c4c6a
Jerome <nietsemorej@gmail.com>
2021-08-31 10:08:05 -0400

    Add comments.

---
+++ Lint-92. Backpack.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    public int backPack(int m, int[] A) {
+        if (A.length == 0 || m == 0) {
+            return 0;
+        }
+
+        int n = A.length;
+        int[] dp = new int[m + 1];
+
+        // Use rolling array to optimize the space complexity,
+        // time complexity is still the same.
+        // Still 2 for loops,
+        // only the second should be inverted order
+        // to make sure dp[j - A[i]] will not be updated in advance wrongly.
+
+        for (int i = 0; i < n; i++) {
+            for (int j = m; j >= A[i]; j--) {
+                dp[j] = Integer.max(dp[j], dp[j - A[i]] + A[i]);
+            }
+        }
+        return dp[m];
+    }
+}
