ab10e876232515d8986095979722d86ebaf6baa0
Jerome <nietsemorej@gmail.com>
2021-08-31 21:27:37 -0400

    37ms, 60%, 49%. rolling array.

--- 474.ones-and-zeroes.java
+++ 474.ones-and-zeroes.java
@@ -22,24 +22,32 @@ class Solution {
             }
         }
 
-        int[][][] dp = new int[strs.length + 1][m + 1][n + 1];
+        // int[][][] dp = new int[strs.length + 1][m + 1][n + 1];
+        int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= strs.length; i++) {
             int zeros = numZeros[i], ones = numOnes[i];
-            // for example, "0", the zeros=1, ones=0, still count 1.
-            // so for j and k, we should start from 0.
-            for (int j = 0; j <= m; j++) {
-                for (int k = 0; k <= n; k++) {
-                    dp[i][j][k] = dp[i - 1][j][k];
-                    if (j >= zeros && k >= ones) {
-                        int notplusItemi = dp[i][j][k];
-                        int plusItemi = dp[i - 1][j - zeros][k - ones] + 1;
-                        dp[i][j][k] = Math.max(notplusItemi, plusItemi);
-                    }
+            // // for example, "0", the zeros=1, ones=0, still count 1.
+            // // so for j and k, we should start from 0.
+            // for (int j = 0; j <= m; j++) {
+            // for (int k = 0; k <= n; k++) {
+            // dp[i][j][k] = dp[i - 1][j][k];
+            // if (j >= zeros && k >= ones) {
+            // int notplusItemi = dp[i][j][k];
+            // int plusItemi = dp[i - 1][j - zeros][k - ones] + 1;
+            // dp[i][j][k] = Math.max(notplusItemi, plusItemi);
+            // }
+            // }
+            // }
+            for (int j = m; j >= zeros; j--) {
+                for (int k = n; k >= ones; k--) {
+                    int notplusItemi = dp[j][k];
+                    int plusItemi = dp[j - zeros][k - ones] + 1;
+                    dp[j][k] = Math.max(notplusItemi, plusItemi);
                 }
             }
         }
 
-        return dp[strs.length][m][n];
+        return dp[m][n];
     }
 }
 // @lc code=end
