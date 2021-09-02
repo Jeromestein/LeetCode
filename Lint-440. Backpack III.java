a69647d26263bf86b78a12ae338d4a9f022887fe
Jeromestein <nietsemorej@gmail.com>
2021-09-02 10:02:15 -0400

    183ms, 51%.

--- Lint-440. Backpack III.java
+++ Lint-440. Backpack III.java
@@ -8,21 +8,24 @@ public class Solution {
     public int backPackIII(int[] A, int[] V, int m) {
         // write your code here
         int n = A.length;
-        int[][] dp = new int[n + 1][m + 1];
+        // int[][] dp = new int[n + 1][m + 1];
 
-        // initialization
-        dp[0][0] = 0;
-
-        // function
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j - A[i - 1] >= 0) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i][j - A[i - 1]] + V[i - 1]);
-                }
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 0; j <= m; j++) {
+        // dp[i][j] = dp[i - 1][j];
+        // if (j - A[i - 1] >= 0) {
+        // dp[i][j] = Math.max(dp[i][j], dp[i][j - A[i - 1]] + V[i - 1]);
+        // }
+        // }
+        // }
+        int[] dp = new int[m + 1];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = A[i]; j <= m; j++) {
+                dp[j] = Math.max(dp[j], dp[j - A[i]] + V[i]);
             }
         }
 
-        return dp[n][m];
+        // return dp[n][m];
+        return dp[m];
     }
 }
