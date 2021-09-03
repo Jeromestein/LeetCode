066bc3cd4276d034d9d27d4c37b3a0bf2f213f8b
Jerome <nietsemorej@gmail.com>
2021-09-02 23:43:13 -0400

    !set.add(nums[i])

--- 219.contains-duplicate-ii.java
+++ 219.contains-duplicate-ii.java
@@ -13,16 +13,13 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i]) && set.size() <= k) {
+            if (!set.add(nums[i])) {
                 return true;
             }
-            if (!set.contains(nums[i])) {
-                set.add(nums[i]);
-            }
+
             if (set.size() > k) {
                 set.remove(nums[i - k]);
             }
-
         }
 
         return false;
