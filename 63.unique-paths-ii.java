eb5d723945a0a68f5e641444f69b79db284774c7
Jerome <nietsemorej@gmail.com>
2021-09-04 17:53:04 -0400

    rolling array

--- 63.unique-paths-ii.java
+++ 63.unique-paths-ii.java
@@ -12,26 +12,20 @@ class Solution {
             return 0;
         }
 
-        int[][] dp = new int[m][n];
+        int[] dp = new int[n];
+        dp[0] = 1;
 
-        for (int i = 0, j = 0; j < n && obstacleGrid[i][j] == 0; j++) {
-            dp[i][j] = 1;
-        }
-        for (int i = 0, j = 0; i < m && obstacleGrid[i][j] == 0; i++) {
-            dp[i][j] = 1;
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
                 }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
 // @lc code=end
