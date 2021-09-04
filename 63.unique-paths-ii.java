ea9675f4430508aa1228dc2b114dc8808da450b6
Jerome <nietsemorej@gmail.com>
2021-09-04 17:20:23 -0400

    my code, 0ms, 100%, 82%.

---
+++ 63.unique-paths-ii.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=63 lang=java
+ *
+ * [63] Unique Paths II
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 0, j = 0; j < n && obstacleGrid[i][j] == 0; j++) {
+            dp[i][j] = 1;
+        }
+        for (int i = 0, j = 0; i < m && obstacleGrid[i][j] == 0; i++) {
+            dp[i][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
+// @lc code=end
