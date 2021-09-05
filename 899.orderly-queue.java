5db0d31a12a8557071a1d3f3231aaa72e491b064
Jerome <nietsemorej@gmail.com>
2021-09-05 11:52:37 -0400

    Add comments

--- 899.orderly-queue.java
+++ 899.orderly-queue.java
@@ -9,17 +9,23 @@
 class Solution {
     public String orderlyQueue(String S, int K) {
         if (K == 1) {
+            // if k==1, then we can consider this queue as a loop.
+            // all we need to do is to find the sexicographically smallest string from the
+            // loop.
             String ans = S;
             for (int i = 0; i < S.length(); ++i) {
+                // T is a the string starting from i
                 String T = S.substring(i) + S.substring(0, i);
                 if (T.compareTo(ans) < 0)
                     ans = T;
             }
             return ans;
         } else {
-            char[] ca = S.toCharArray();
-            Arrays.sort(ca);
-            return new String(ca);
+            // if k>1, then we can swap any two letters in the queue.
+            // so just sort it.
+            char[] charArray = S.toCharArray();
+            Arrays.sort(charArray);
+            return new String(charArray);
         }
     }
 }
