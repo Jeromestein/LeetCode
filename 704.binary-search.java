7621a3b64674eb65da9981f8977dad85f70acdc2
Jerome <nietsemorej@gmail.com>
2021-09-05 12:15:45 -0400

    Add comments

--- 704.binary-search.java
+++ 704.binary-search.java
@@ -6,19 +6,25 @@
  */
 
 // @lc code=start
+
 class Solution {
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+
+        // If target exists, then return its index. Otherwise, return -1.
+        // if use while (left = right), when n=1, it will skip the loop.
+        // so it must be while (left <= right)
         while (left <= right) {
-            int pivot = left + (right - left) / 2;
-            if (nums[pivot] == target) {
-                return pivot;
-            } else if (nums[pivot] > target) {
-                right = pivot - 1;
-            } else if (nums[pivot] < target) {
-                left = pivot + 1;
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             }
         }
+
         return -1;
     }
 }
