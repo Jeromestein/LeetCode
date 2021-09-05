e142eeb25530cbb858386b7ac3cc24799260123d
Jerome <nietsemorej@gmail.com>
2021-09-05 11:48:33 -0400

    2ms, 89%, 50%.

---
+++ 899.orderly-queue.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=899 lang=java
+ *
+ * [899] Orderly Queue
+ */
+
+// @lc code=start
+
+class Solution {
+    public String orderlyQueue(String S, int K) {
+        if (K == 1) {
+            String ans = S;
+            for (int i = 0; i < S.length(); ++i) {
+                String T = S.substring(i) + S.substring(0, i);
+                if (T.compareTo(ans) < 0)
+                    ans = T;
+            }
+            return ans;
+        } else {
+            char[] ca = S.toCharArray();
+            Arrays.sort(ca);
+            return new String(ca);
+        }
+    }
+}
+
+// @lc code=end
