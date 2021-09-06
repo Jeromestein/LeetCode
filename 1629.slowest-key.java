23de0f1405e9de69137d8728ff2d892393ff7099
Jerome <nietsemorej@gmail.com>
2021-09-06 08:40:13 -0400

    clean code

--- 1629.slowest-key.java
+++ 1629.slowest-key.java
@@ -5,33 +5,22 @@
  */
 
 // @lc code=start
+
 class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        // keysPressed contains only lowercase English letters.
-        int[] duration = new int[26];
+        int longest = releaseTimes[0];
+        char slowestKey = keysPressed.charAt(0);
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int currDuration = releaseTimes[i] - releaseTimes[i - 1];
+            char currKey = keysPressed.charAt(i);
 
-        // get max duration of each letter
-        for (int i = 0; i < keysPressed.length(); i++) {
-            int currLetter = keysPressed.charAt(i) - 'a';
-            int currDuration = 0;
-            if (i == 0) {
-                currDuration = releaseTimes[i];
-            } else {
-                currDuration = releaseTimes[i] - releaseTimes[i - 1];
-            }
-            duration[currLetter] = Math.max(duration[currLetter], currDuration);
-        }
-
-        int longest = 0;
-        char res = 'a';
-        for (int i = duration.length - 1; i >= 0; i--) {
-            if (longest < duration[i]) {
-                longest = duration[i];
-                res = (char) (i + 'a');
+            if (currDuration > longest || currDuration == longest && currKey > slowestKey) {
+                longest = currDuration;
+                slowestKey = currKey;
             }
         }
 
-        return res;
+        return slowestKey;
     }
 }
 // @lc code=end
