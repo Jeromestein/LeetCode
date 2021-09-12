4f7703bbeda5660367466398feac497c0de54f44
Jerome <nietsemorej@gmail.com>
2021-09-12 15:02:55 -0400

    Add comments

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -9,33 +9,33 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int pivot = left + (right - left) / 2;
-            if (leftDerivative(nums, pivot) == 1 && rightDerivative(nums, pivot) == -1) {
-                return pivot;
-            } else if (leftDerivative(nums, pivot) == 1 && rightDerivative(nums, pivot) == 1) {
-                left = pivot + 1;
+            int mid = left + (right - left) / 2;
+            if (leftDerivative(nums, mid) == 1 && rightDerivative(nums, mid) == -1) {
+                return mid;
+            } else if (leftDerivative(nums, mid) == 1 && rightDerivative(nums, mid) == 1) {
+                left = mid + 1;
             } else {
-                right = pivot - 1;
+                right = mid - 1;
             }
         }
-        throw new IllegalArgumentException("No Solution");
+        return -1;
     }
 
     // 1 : +
     // -1: -
     // 0 : 0
+    // nums[i] != nums[i + 1]
+    // nums[-1] = nums[n] = -∞.
     public int leftDerivative(int[] nums, int index) {
         if (index == 0) {
             return 1;
         }
+
         if (nums[index] > nums[index - 1]) {
             return 1;
-        } else if (nums[index] < nums[index - 1]) {
+        } else {
             return -1;
-        } else if (nums[index] == nums[index - 1]) {
-            return 0;
         }
-        throw new IllegalArgumentException("No Solution");
     }
 
     public int rightDerivative(int[] nums, int index) {
@@ -44,12 +44,9 @@ class Solution {
         }
         if (nums[index] > nums[index + 1]) {
             return -1;
-        } else if (nums[index] < nums[index + 1]) {
-            return +1;
-        } else if (nums[index] == nums[index + 1]) {
-            return 0;
+        } else {
+            return 1;
         }
-        throw new IllegalArgumentException("No Solution");
     }
 }
 // @lc code=end
