8bfb71b2745103f34e998e5bb964ac2f47dd7ec6
Jerome <nietsemorej@gmail.com>
2021-09-13 09:23:20 -0400

    Add comments

--- 302. smallest-rectangle-enclosing-black-pixels.java
+++ 302. smallest-rectangle-enclosing-black-pixels.java
@@ -48,22 +48,30 @@
 //     }
 // }
 
-public class Solution {
+class Solution {
     public int minArea(char[][] image, int x, int y) {
         int m = image.length, n = image[0].length;
+        // binary search [0, y) to find left
         int left = searchColumns(image, 0, y, 0, m, true);
+        // binary search [y+1, n) to find right
         int right = searchColumns(image, y + 1, n, 0, m, false);
+        // binary search [0, x) to find top
         int top = searchRows(image, 0, x, left, right, true);
+        // binary search [x+1, m) to find bottom
         int bottom = searchRows(image, x + 1, m, left, right, false);
         return (right - left) * (bottom - top);
     }
 
     private int searchColumns(char[][] image, int i, int j, int top, int bottom, boolean whiteToBlack) {
         while (i != j) {
-            int k = top, mid = (i + j) / 2;
+            int mid = (i + j) / 2;
+
+            // frome top to bottom, stop when image[k][mid] == '1'
+            int k = top;
             while (k < bottom && image[k][mid] == '0')
-                ++k;
-            if (k < bottom == whiteToBlack) // k < bottom means the column mid has black pixel
+                k++;
+
+            if (k < bottom == whiteToBlack) // k < bottom == true means the column mid has black pixel
                 j = mid; // search the boundary in the smaller half
             else
                 i = mid + 1; // search the boundary in the greater half
@@ -73,10 +81,14 @@ public class Solution {
 
     private int searchRows(char[][] image, int i, int j, int left, int right, boolean whiteToBlack) {
         while (i != j) {
-            int k = left, mid = (i + j) / 2;
+            int mid = (i + j) / 2;
+
+            // frome left to right, stop when image[mid][k] == '1'
+            int k = left;
             while (k < right && image[mid][k] == '0')
-                ++k;
-            if (k < right == whiteToBlack) // k < right means the row mid has black pixel
+                k++;
+
+            if (k < right == whiteToBlack) // k < right == true means the row mid has black pixel
                 j = mid;
             else
                 i = mid + 1;
