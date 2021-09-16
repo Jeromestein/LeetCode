b2ad0cb8b8884c436159dcd7c1ffaa3912393499
Jeromestein <nietsemorej@gmail.com>
2021-09-16 15:36:48 -0400

    math

--- 292.nim-game.java
+++ 292.nim-game.java
@@ -7,16 +7,6 @@
 // @lc code=start
 class Solution {
     public boolean canWinNim(int n) {
-        if (n < 4)
-            return true;
-        // 1<=n<=2^31-1
-        // boolean[] dp = new boolean[n + 1];
-        // dp[1] = dp[2] = dp[3] = true;
-
-        // for (int i = 4; i <= n; i++) {
-        // dp[i] = !dp[i - 1] || !dp[i - 2] || !dp[i - 3];
-        // }
-
         // return dp[n];
         return n % 4 != 0;
     }
