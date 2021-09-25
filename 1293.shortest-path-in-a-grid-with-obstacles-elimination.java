f3cf9a5a0a17f7b5c26255221307dee0d3c9bafe
Jerome <nietsemorej@gmail.com>
2021-09-25 09:39:53 -0400

    27ms, 44%, 32%.

--- 1293.shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293.shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -5,56 +5,60 @@
  */
 
 // @lc code=start
+
 class Solution {
-    int minSteps = Integer.MAX_VALUE;
-    boolean[][] footPrint;
-    // 4 directions
-    int[] deltaI = { 1, -1, 0, 0 };
-    int[] deltaJ = { 0, 0, 1, -1 };
-
     public int shortestPath(int[][] grid, int k) {
-        int m = grid.length, n = grid[0].length;
-        footPrint = new boolean[m][n];
-
-        footPrint[0][0] = true;
-        dfs(grid, k, 0, 0, 0);
-
-        if (minSteps == Integer.MAX_VALUE)
-            return -1;
-        else
-            return minSteps;
-    }
-
-    public void dfs(int[][] grid, int currk, int i, int j, int currSteps) {
-        int m = grid.length, n = grid[0].length;
-        if (i == m - 1 && j == n - 1) {
-            minSteps = Math.min(minSteps, currSteps);
-            return;
+        int row = grid.length, col = grid[0].length;
+        if (row == 1 && col == 1) {
+            return 0;
         }
 
-        // go through all the directions
-        for (int k = 0; k < 4; k++) {
-            int ni = i + deltaI[k], nj = j + deltaJ[k];
-            // make sure its int the matrix
-            if (0 <= ni && ni < m && 0 <= nj && nj < n) {
-                // make sure we never pass here
-                if (!footPrint[ni][nj]) {
-                    // make sure if its obstacle
-                    if (grid[ni][nj] == 0) {
-                        footPrint[ni][nj] = true;
-                        dfs(grid, currk, ni, nj, currSteps + 1);
-                        footPrint[ni][nj] = false;
-                    }
-                    if (grid[ni][nj] == 1 && currk > 0) {
-                        //
-                        footPrint[ni][nj] = true;
-                        dfs(grid, currk - 1, ni, nj, currSteps + 1);
-                        footPrint[ni][nj] = false;
+        // direction matrix
+        int[][] dirs = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+        Deque<int[]> queue = new LinkedList<>();
+        int[][] visited = new int[row][col];
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                visited[i][j] = -1;
+            }
+        }
+
+        queue.offerLast(new int[] { 0, 0, k });
+        visited[0][0] = k;
+        int step = 0;
+
+        while (!queue.isEmpty()) {
+            int currSize = queue.size();
+            step++;
+            for (int i = 0; i < currSize; ++i) {
+                int[] currPos = queue.pollFirst();
+
+                for (int j = 0; j < 4; ++j) {
+                    int nx = currPos[0] + dirs[j][0];
+                    int ny = currPos[1] + dirs[j][1];
+                    int currK = currPos[2];
+                    // 保证下一步的位置在矩阵中
+                    if (nx >= 0 && nx < row && ny >= 0 && ny < col) {
+                        // 判断是否到达右下角
+                        // 题目中明确了 grid[0][0] == grid[m-1][n-1] == 0，若目标点可以为1，需要修改此部分
+                        if (nx == row - 1 && ny == col - 1) {
+                            return step;
+                        }
+
+                        currK = grid[nx][ny] == 0 ? currK : --currK;
+
+                        if (currK >= 0) {
+                            if (visited[nx][ny] == -1 || (visited[nx][ny] != -1 && currK > visited[nx][ny])) {
+                                queue.offerLast(new int[] { nx, ny, currK });
+                                visited[nx][ny] = currK;
+                            }
+                        }
                     }
                 }
             }
         }
-
+        return -1;
     }
 }
+
 // @lc code=end
