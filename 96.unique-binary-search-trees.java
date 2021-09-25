4f420dbae9def0ed91cecc7d1b616baa3bfd4d88
Jerome <nietsemorej@gmail.com>
2021-09-25 10:45:00 -0400

    0ms, 100%, 80%. dp.

--- 96.unique-binary-search-trees.java
+++ 96.unique-binary-search-trees.java
@@ -6,14 +6,29 @@
 
 // @lc code=start
 
+// class Solution {
+//     public int numTrees(int n) {
+//         // use long to avoid overflow
+//         long C = 1;
+//         for (int i = 0; i < n; ++i) {
+//             C = C * 2 * (2 * i + 1) / (i + 2);
+//         }
+//         return (int) C;
+//     }
+// }
+
 class Solution {
     public int numTrees(int n) {
-        // use long to avoid overflow
-        long C = 1;
-        for (int i = 0; i < n; ++i) {
-            C = C * 2 * (2 * i + 1) / (i + 2);
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
         }
-        return (int) C;
+        return G[n];
     }
 }
 
