98aa812b2d2db30e5359d7ca9441de3f4ff225cf
Jeromestein <nietsemorej@gmail.com>
2021-09-30 09:47:57 -0400

    0ms, 100%, 70%. backtracing.

--- 698.partition-to-k-equal-sum-subsets.java
+++ 698.partition-to-k-equal-sum-subsets.java
@@ -5,58 +5,79 @@
  */
 
 // @lc code=start
-
 class Solution {
+    private boolean backtrack(int[] arr, int index, int count, int currSum, int k, int targetSum, boolean[] taken) {
 
-    public boolean canPartitionKSubsets(int[] nums, int k) {
-        if (k == 1) {
+        int n = arr.length;
+
+        // We made k - 1 subsets with target sum and last subset will also have target
+        // sum.
+        if (count == k - 1) {
             return true;
         }
 
-        int len = nums.length;
-        Arrays.sort(nums);
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        if (sum % k != 0) {
-            return false;
-        }
-        int target = sum / k;
-        if (nums[len - 1] > target) {
+        // No need to proceed further.
+        if (currSum > targetSum) {
             return false;
         }
 
-        int size = 1 << len;
-        boolean[] dp = new boolean[size];
-        dp[0] = true;
-        int[] currentSum = new int[size];
-        for (int i = 0; i < size; i++) {
-            // 总是基于 dp[i] = true 的前提下进行状态转移
-            if (!dp[i]) {
-                continue;
-            }
+        // When curr sum reaches target then one subset is made.
+        // Increment count and reset current sum.
+        if (currSum == targetSum) {
+            return backtrack(arr, 0, count + 1, 0, k, targetSum, taken);
+        }
 
-            // 基于当前状态，添加一个数以后
-            for (int j = 0; j < len; j++) {
-                if ((i & (1 << j)) != 0) {
-                    continue;
+        // Try not picked elements to make some combinations.
+        for (int j = index; j < n; ++j) {
+            if (!taken[j]) {
+                // Include this element in current subset.
+                taken[j] = true;
+
+                // If using current jth element in this subset leads to make all valid subsets.
+                if (backtrack(arr, j + 1, count, currSum + arr[j], k, targetSum, taken)) {
+                    return true;
                 }
-                int next = i | (1 << j);
-                if (dp[next]) {
-                    continue;
-                }
-                if ((currentSum[i] % target) + nums[j] <= target) {
-                    currentSum[next] = currentSum[i] + nums[j];
-                    dp[next] = true;
-                } else {
-                    // 由于数组已经排好序，如果 (currentSum[i] % target) + nums[j] > target，剩下的数就没有必要枚举
-                    break;
-                }
+
+                // Backtrack step.
+                taken[j] = false;
             }
         }
-        return dp[size - 1];
+
+        // We were not able to make a valid combination after picking each element from
+        // the array,
+        // hence we can't make k subsets.
+        return false;
+    }
+
+    void reverse(int[] arr) {
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public boolean canPartitionKSubsets(int[] arr, int k) {
+        int totalArraySum = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n; ++i) {
+            totalArraySum += arr[i];
+        }
+
+        // If total sum not divisible by k, we can't make subsets.
+        if (totalArraySum % k != 0) {
+            return false;
+        }
+
+        // Sort in decreasing order.
+        Arrays.sort(arr);
+        reverse(arr);
+
+        int targetSum = totalArraySum / k;
+        boolean[] taken = new boolean[n];
+
+        return backtrack(arr, 0, 0, 0, k, targetSum, taken);
     }
 }
-
 // @lc code=end
