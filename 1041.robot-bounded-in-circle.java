b6016b8f5273afe1d467e8f7081cd00b990e5399
Jerome <nietsemorej@gmail.com>
2021-10-04 22:35:58 -0400

    1ms, 40%, 18%.

---
+++ 1041.robot-bounded-in-circle.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1041 lang=java
+ *
+ * [1041] Robot Bounded In Circle
+ */
+
+// @lc code=start
+
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int[][] dirs = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        // Initial position is in the center
+        int x = 0, y = 0;
+        // facing north
+        int idx = 0;
+
+        for (char i : instructions.toCharArray()) {
+            switch (i) {
+                case 'L':
+                    idx = (idx + 3) % 4;
+                    break;
+                case 'R':
+                    idx = (idx + 1) % 4;
+                    break;
+                case 'G':
+                    x += dirs[idx][0];
+                    y += dirs[idx][1];
+                    break;
+            }
+
+        }
+
+        // after one cycle:
+        // robot returns into initial position
+        // or robot doesn't face north
+        return (x == 0 && y == 0) || (idx != 0);
+    }
+}
+
+// @lc code=end
