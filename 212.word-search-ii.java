75e1eb37887a0ada334736b36db307a6d37f104b
Jerome <nietsemorej@gmail.com>
2021-10-08 21:49:41 -0400

    15ms, 97%, 35%. backtracing + trie.

--- 212.word-search-ii.java
+++ 212.word-search-ii.java
@@ -6,55 +6,84 @@
 
 // @lc code=start
 
+class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+    String word = null;
+
+    public TrieNode() {
+    }
+}
+
 class Solution {
-    private char[][] board;
-    private int ROWS;
-    private int COLS;
+    char[][] _board = null;
+    ArrayList<String> _result = new ArrayList<String>();
 
     public List<String> findWords(char[][] board, String[] words) {
-        this.board = board;
-        this.ROWS = board.length;
-        this.COLS = board[0].length;
-        List<String> res = new ArrayList<>();
 
-        for (int row = 0; row < this.ROWS; ++row) {
-            for (int col = 0; col < this.COLS; ++col) {
-                for (String word : words) {
-                    if (!res.contains(word) && this.backtrack(row, col, word, 0)) {
-                        res.add(word);
-                        continue;
-                    }
+        // Step 1). Construct the Trie
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            TrieNode node = root;
+
+            for (Character letter : word.toCharArray()) {
+                if (node.children.containsKey(letter)) {
+                    node = node.children.get(letter);
+                } else {
+                    TrieNode newNode = new TrieNode();
+                    node.children.put(letter, newNode);
+                    node = newNode;
+                }
+            }
+            node.word = word; // store words in Trie
+        }
+
+        this._board = board;
+        // Step 2). Backtracking starting for each cell in the board
+        for (int row = 0; row < board.length; ++row) {
+            for (int col = 0; col < board[row].length; ++col) {
+                if (root.children.containsKey(board[row][col])) {
+                    backtracking(row, col, root);
                 }
             }
         }
-        return res;
+
+        return this._result;
     }
 
-    protected boolean backtrack(int row, int col, String word, int index) {
-        /* Step 1). check the bottom case. */
-        if (index >= word.length())
-            return true;
+    private void backtracking(int row, int col, TrieNode parent) {
+        Character letter = this._board[row][col];
+        TrieNode currNode = parent.children.get(letter);
 
-        /* Step 2). Check the boundaries. */
-        if (row < 0 || row == this.ROWS || col < 0 || col == this.COLS || this.board[row][col] != word.charAt(index))
-            return false;
-
-        /* Step 3). explore the neighbors in DFS */
-        boolean ret = false;
-        // mark the path before the next exploration
-        this.board[row][col] = '#';
-
-        int[] rowOffsets = { 0, 1, 0, -1 };
-        int[] colOffsets = { 1, 0, -1, 0 };
-        for (int d = 0; d < 4; ++d) {
-            ret = this.backtrack(row + rowOffsets[d], col + colOffsets[d], word, index + 1);
-            if (ret)
-                break;
+        // check if there is any match
+        if (currNode.word != null) {
+            this._result.add(currNode.word);
+            currNode.word = null;
         }
 
-        /* Step 4). clean up and return the result. */
-        this.board[row][col] = word.charAt(index);
-        return ret;
+        // mark the current letter before the EXPLORATION
+        this._board[row][col] = '#';
+
+        // explore neighbor cells in around-clock directions: up, right, down, left
+        int[] rowOffset = { -1, 0, 1, 0 };
+        int[] colOffset = { 0, 1, 0, -1 };
+        for (int i = 0; i < 4; ++i) {
+            int newRow = row + rowOffset[i];
+            int newCol = col + colOffset[i];
+            if (newRow < 0 || newRow >= this._board.length || newCol < 0 || newCol >= this._board[0].length) {
+                continue;
+            }
+            if (currNode.children.containsKey(this._board[newRow][newCol])) {
+                backtracking(newRow, newCol, currNode);
+            }
+        }
+
+        // End of EXPLORATION, restore the original letter in the board.
+        this._board[row][col] = letter;
+
+        // Optimization: incrementally remove the leaf nodes
+        if (currNode.children.isEmpty()) {
+            parent.children.remove(letter);
+        }
     }
 }
 
