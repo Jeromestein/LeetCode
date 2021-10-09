b0d0c3359dfc40e27e80139a64ae3142a3df2b70
Jerome <nietsemorej@gmail.com>
2021-10-08 21:48:59 -0400

    my code, 2348ms, 5%, 25%...

---
+++ 212.word-search-ii.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=212 lang=java
+ *
+ * [212] Word Search II
+ */
+
+// @lc code=start
+
+class Solution {
+    private char[][] board;
+    private int ROWS;
+    private int COLS;
+
+    public List<String> findWords(char[][] board, String[] words) {
+        this.board = board;
+        this.ROWS = board.length;
+        this.COLS = board[0].length;
+        List<String> res = new ArrayList<>();
+
+        for (int row = 0; row < this.ROWS; ++row) {
+            for (int col = 0; col < this.COLS; ++col) {
+                for (String word : words) {
+                    if (!res.contains(word) && this.backtrack(row, col, word, 0)) {
+                        res.add(word);
+                        continue;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    protected boolean backtrack(int row, int col, String word, int index) {
+        /* Step 1). check the bottom case. */
+        if (index >= word.length())
+            return true;
+
+        /* Step 2). Check the boundaries. */
+        if (row < 0 || row == this.ROWS || col < 0 || col == this.COLS || this.board[row][col] != word.charAt(index))
+            return false;
+
+        /* Step 3). explore the neighbors in DFS */
+        boolean ret = false;
+        // mark the path before the next exploration
+        this.board[row][col] = '#';
+
+        int[] rowOffsets = { 0, 1, 0, -1 };
+        int[] colOffsets = { 1, 0, -1, 0 };
+        for (int d = 0; d < 4; ++d) {
+            ret = this.backtrack(row + rowOffsets[d], col + colOffsets[d], word, index + 1);
+            if (ret)
+                break;
+        }
+
+        /* Step 4). clean up and return the result. */
+        this.board[row][col] = word.charAt(index);
+        return ret;
+    }
+}
+
+// @lc code=end
