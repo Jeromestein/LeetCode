cead8ae9f9001e2c53db900207c332a7a17671a0
Jerome <nietsemorej@gmail.com>
2021-10-12 10:12:02 -0400

    Add comments

--- 1153.string-transforms-into-another-string.java
+++ 1153.string-transforms-into-another-string.java
@@ -7,16 +7,20 @@
 // @lc code=start
 
 class Solution {
-    public boolean canConvert(String s1, String s2) {
-        if (s1.equals(s2))
+    public boolean canConvert(String str1, String str2) {
+        if (str1.equals(str2))
             return true;
-        Map<Character, Character> dp = new HashMap<>();
-        for (int i = 0; i < s1.length(); ++i) {
-            if (dp.getOrDefault(s1.charAt(i), s2.charAt(i)) != s2.charAt(i))
+        Map<Character, Character> map = new HashMap<>();
+
+        int len = str1.length();
+        for (int i = 0; i < len; i++) {
+            char ch1 = str1.charAt(i), ch2 = str2.charAt(i);
+            if (map.putIfAbsent(ch1, ch2) != null && ch2 != map.putIfAbsent(ch1, ch2)) {
                 return false;
-            dp.put(s1.charAt(i), s2.charAt(i));
+            }
         }
-        return new HashSet<Character>(dp.values()).size() < 26;
+
+        return new HashSet<Character>(map.values()).size() < 26;
     }
 }
 // @lc code=end
