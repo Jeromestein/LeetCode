eb7debc0ffd153dff7bece03846300c14451fdd2
Jerome <nietsemorej@gmail.com>
2021-10-13 10:43:43 -0400

    Add comments

--- 1857.largest-color-value-in-a-directed-graph.java
+++ 1857.largest-color-value-in-a-directed-graph.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 /*
  * @lc app=leetcode id=1857 lang=java
  *
@@ -20,45 +23,51 @@ class Solution {
             int u = edge[0];
             int v = edge[1];
             indegree[v]++;
+            // directed graph
             graph.get(u).add(v);
         }
 
-        // 2. color count map. map[i][j] is the maximum count of j-th color from the
-        // ancester nodes to node i
-        int[][] map = new int[n][26];
+        // 2. color count map. dp
+        // dp[i][j] is the maximum count of j color from the ancester nodes to node i
+        int[][] dp = new int[n][26];
 
-        // 3.Khans Algorithm ( Iterative Topological Sort)
-        Queue<Integer> que = new ArrayDeque<>();
+        // 3.Khans Algorithm (Iterative Topological Sort)
+        // detect cycle and fill out dp[][]
+        Deque<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             if (indegree[i] == 0) {
-                que.add(i);
-                map[i][color[i] - 'a'] = 1;
+                q.add(i);
+                dp[i][color[i] - 'a'] = 1;
             }
         }
 
         int res = 0;
         int seen = 0;
 
-        while (que.size() > 0) {
-            int node = que.remove();
+        while (q.size() > 0) {
+            int currNode = q.pop();
             seen++;
 
-            int max = getMax(map[node]);
+            int max = getMax(dp[currNode]);
             res = Math.max(res, max);
 
-            for (int nbr : graph.get(node)) {
+            for (int nextNode : graph.get(currNode)) {
                 // update the map of next node
                 for (int i = 0; i < 26; i++) {
-                    map[nbr][i] = Math.max(map[nbr][i], map[node][i] + (color[nbr] - 'a' == i ? 1 : 0));
+                    int isColori = color[nextNode] - 'a' == i ? 1 : 0;
+                    dp[nextNode][i] = Math.max(dp[nextNode][i], dp[currNode][i] + isColori);
                 }
-                indegree[nbr]--;
+                indegree[nextNode]--;
 
-                if (indegree[nbr] == 0) {
-                    que.add(nbr);
+                if (indegree[nextNode] == 0) {
+                    q.add(nextNode);
                 }
             }
         }
         // if seen!=n means cycle is there
+        // "a"
+        // [[0,0]]
+        // n=0 != 1
         return seen == n ? res : -1;
     }
 
