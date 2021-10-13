ab546dccf8c44ceaf23a746c8d51013a97860d57
Jerome <nietsemorej@gmail.com>
2021-10-13 09:48:45 -0400

    Add comments

--- 1048.longest-string-chain.java
+++ 1048.longest-string-chain.java
@@ -8,6 +8,7 @@
 
 class Solution {
     public int longestStrChain(String[] words) {
+        int n = words.length;
         Map<String, Integer> dp = new HashMap<>();
 
         // Sorting the list in terms of the word length.
@@ -15,15 +16,16 @@ class Solution {
 
         int longestWordSequenceLength = 1;
 
-        for (String word : words) {
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
             int presentLength = 1;
             // Find all possible predecessors for the current word by removing one letter at
             // a time.
-            for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < word.length(); j++) {
                 StringBuilder temp = new StringBuilder(word);
-                temp.deleteCharAt(i);
-                String predecessor = temp.toString();
-                int previousLength = dp.getOrDefault(predecessor, 0);
+                temp.deleteCharAt(j);
+
+                int previousLength = dp.getOrDefault(temp.toString(), 0);
                 presentLength = Math.max(presentLength, previousLength + 1);
             }
             dp.put(word, presentLength);
