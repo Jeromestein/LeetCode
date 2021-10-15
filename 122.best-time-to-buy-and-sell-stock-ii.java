ecc6ab3535e619b4b000115e6cc594536d792e5d
Jerome <nietsemorej@gmail.com>
2021-10-15 09:31:56 -0400

    0ms, 100%, 52%. greedy.

--- 122.best-time-to-buy-and-sell-stock-ii.java
+++ 122.best-time-to-buy-and-sell-stock-ii.java
@@ -6,30 +6,41 @@
 
 // @lc code=start
 
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int maxProfit = 0;
+
+//         int bottom = 0, peak = 0;
+//         // basically, find the rising edge
+//         while (peak < n && bottom < n - 1) {
+//             // 1. find the bottom
+//             while (bottom + 1 < n && prices[bottom] >= prices[bottom + 1]) {
+//                 bottom++;
+//             }
+//             // 2. find the peak
+//             peak = bottom;
+//             while (peak + 1 < n && prices[peak] <= prices[peak + 1]) {
+//                 peak++;
+//             }
+
+//             maxProfit += prices[peak] - prices[bottom];
+
+//             bottom = peak;
+//         }
+
+//         return maxProfit;
+//     }
+// }
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int maxProfit = 0;
-
-        int bottom = 0, peak = 0;
-        // basically, find the rising edge
-        while (peak < n && bottom < n - 1) {
-            // 1. find the bottom
-            while (bottom + 1 < n && prices[bottom] >= prices[bottom + 1]) {
-                bottom++;
-            }
-            // 2. find the peak
-            peak = bottom;
-            while (peak + 1 < n && prices[peak] <= prices[peak + 1]) {
-                peak++;
-            }
-
-            maxProfit += prices[peak] - prices[bottom];
-
-            bottom = peak;
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
         }
-
-        return maxProfit;
+        return maxprofit;
     }
 }
 // @lc code=end
