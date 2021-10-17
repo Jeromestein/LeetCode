524ff175c0d0da517116c7a4bef741701037347b
Jerome <nietsemorej@gmail.com>
2021-10-16 21:53:21 -0400

    9ms, 98%, 46%. union find.

--- 886.possible-bipartition.java
+++ 886.possible-bipartition.java
@@ -7,37 +7,48 @@
 // @lc code=start
 
 class Solution {
-    public boolean possibleBipartition(int N, int[][] dislikes) {
-        // group[i] = 0 means node i hasn't been visited.
-        // group[i] = 1 means node i has been grouped to 1.
-        // group[i] = -1 means node i has been grouped to -1.
-        int[][] graph = new int[N][N];
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        Unionfind uf = new Unionfind(n << 1);
+        int cnt = 0;
         for (int[] d : dislikes) {
-            graph[d[0] - 1][d[1] - 1] = 1;
-            graph[d[1] - 1][d[0] - 1] = 1;
+            int x = d[0];
+            int y = d[1];
+            uf.union(x, y + n);
+            uf.union(x + n, y);
         }
-        int[] group = new int[N];
-        for (int i = 0; i < N; i++) {
-            if (group[i] == 0 && !dfs(graph, group, i, 1)) {
+        for (int i = 1; i <= n; i++) {
+            if (uf.find(i) == uf.find(i + n)) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(int[][] graph, int[] group, int index, int g) {
-        group[index] = g;
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[index][i] == 1) {
-                if (group[i] == g) {
-                    return false;
-                }
-                if (group[i] == 0 && !dfs(graph, group, i, -g)) {
-                    return false;
-                }
-            }
+}
+
+class Unionfind {
+    int[] root;
+
+    public Unionfind(int n) {
+        root = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            root[i] = i;
         }
-        return true;
+    }
+
+    public int find(int x) {
+        if (root[x] == x) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    public void union(int a, int b) {
+        int aa = find(a);
+        int bb = find(b);
+        if (aa != bb) {
+            root[aa] = bb;
+        }
     }
 }
 
