174f4d10c94077d339b0fb64ac904014849a32a0
Jerome <nietsemorej@gmail.com>
2021-10-18 17:46:28 -0400

    Add comments

--- 269.alien-dictionary.java
+++ 269.alien-dictionary.java
@@ -1,3 +1,5 @@
+import java.util.Deque;
+
 /*
  * @lc app=leetcode id=269 lang=java
  *
@@ -5,15 +7,24 @@
  */
 
 // @lc code=start
-class Solution {
-    public String alienOrder(String[] words) {
 
+class Solution {
+    // 1. Extracting dependency rules from the input. For example "A must be before
+    // C",
+    // "X must be before D", or "E must be before B".
+    // 2. Putting the dependency rules into a graph with letters as nodes and
+    // dependencies as edges (an adjacency list is best).
+    // 3. Topologically sorting the graph nodes.
+
+    public String alienOrder(String[] words) {
         // Step 0: Create data structures and find all unique letters.
+        // adjacent list
         Map<Character, List<Character>> adjList = new HashMap<>();
-        Map<Character, Integer> counts = new HashMap<>();
+        // indegree
+        Map<Character, Integer> inDegree = new HashMap<>();
         for (String word : words) {
             for (char c : word.toCharArray()) {
-                counts.put(c, 0);
+                inDegree.put(c, 0);
                 adjList.put(c, new ArrayList<>());
             }
         }
@@ -23,6 +34,7 @@ class Solution {
             String word1 = words[i];
             String word2 = words[i + 1];
             // Check that word2 is not a prefix of word1.
+            // which means a letter is samller than '', impossibel
             if (word1.length() > word2.length() && word1.startsWith(word2)) {
                 return "";
             }
@@ -30,7 +42,7 @@ class Solution {
             for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
                 if (word1.charAt(j) != word2.charAt(j)) {
                     adjList.get(word1.charAt(j)).add(word2.charAt(j));
-                    counts.put(word2.charAt(j), counts.get(word2.charAt(j)) + 1);
+                    inDegree.put(word2.charAt(j), inDegree.get(word2.charAt(j)) + 1);
                     break;
                 }
             }
@@ -38,26 +50,28 @@ class Solution {
 
         // Step 2: Breadth-first search.
         StringBuilder sb = new StringBuilder();
-        Queue<Character> queue = new LinkedList<>();
-        for (Character c : counts.keySet()) {
-            if (counts.get(c).equals(0)) {
-                queue.add(c);
+        Deque<Character> q = new LinkedList<>();
+        for (Character c : inDegree.keySet()) {
+            if (inDegree.get(c).equals(0)) {
+                q.add(c);
             }
         }
-        while (!queue.isEmpty()) {
-            Character c = queue.remove();
+        while (!q.isEmpty()) {
+            Character c = q.poll();
             sb.append(c);
             for (Character next : adjList.get(c)) {
-                counts.put(next, counts.get(next) - 1);
-                if (counts.get(next).equals(0)) {
-                    queue.add(next);
+                inDegree.put(next, inDegree.get(next) - 1);
+                if (inDegree.get(next).equals(0)) {
+                    q.add(next);
                 }
             }
         }
 
-        if (sb.length() < counts.size()) {
+        // the size of indegree is the number of letters
+        if (sb.length() < inDegree.size()) {
             return "";
         }
+
         return sb.toString();
     }
 }
