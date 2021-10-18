5857ff7678ae3114335b2019badf9db2e9a00adb
Jerome <nietsemorej@gmail.com>
2021-10-18 16:09:04 -0400

    my code, 1ms, 54%, 98%. bfs.

--- 938.range-sum-of-bst.java
+++ 938.range-sum-of-bst.java
@@ -12,33 +12,75 @@
  * = left; this.right = right; } }
  */
 
+// class Solution {
+// int sum, low, high;
+
+// public int rangeSumBST(TreeNode root, int low, int high) {
+// sum = 0;
+// this.low = low;
+// this.high = high;
+
+// dfs(root);
+
+// return sum;
+// }
+
+// public void dfs(TreeNode root) {
+// if (root == null)
+// return;
+
+// if (root.val < low) {
+// dfs(root.right);
+// } else if (root.val > high) {
+// dfs(root.left);
+// } else {
+// sum += root.val;
+// dfs(root.left);
+// dfs(root.right);
+// }
+
+// }
+// }
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
 class Solution {
-    int sum, low, high;
-
     public int rangeSumBST(TreeNode root, int low, int high) {
-        sum = 0;
-        this.low = low;
-        this.high = high;
-
-        dfs(root);
-
-        return sum;
-    }
-
-    public void dfs(TreeNode root) {
         if (root == null)
-            return;
+            return 0;
 
-        if (root.val < low) {
-            dfs(root.right);
-        } else if (root.val > high) {
-            dfs(root.left);
-        } else {
-            sum += root.val;
-            dfs(root.left);
-            dfs(root.right);
+        Deque<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        int sum = 0;
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            if (curr.val < low) {
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            } else if (curr.val > high) {
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+            } else {
+                sum += curr.val;
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+
         }
 
+        return sum;
     }
 }
 
