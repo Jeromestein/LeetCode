5ee83fbf4c3a3ff17173a46e517e69b4698bb707
Jerome <nietsemorej@gmail.com>
2021-10-24 18:16:10 -0400

    Add comments

--- 222.count-complete-tree-nodes.java
+++ 222.count-complete-tree-nodes.java
@@ -18,7 +18,7 @@ class Solution {
         int d = 0;
         while (node.left != null) {
             node = node.left;
-            ++d;
+            d++;
         }
         return d;
     }
@@ -28,9 +28,9 @@ class Solution {
     // Binary search with O(d) complexity.
     public boolean exists(int idx, int d, TreeNode node) {
         int left = 0, right = (int) Math.pow(2, d) - 1;
-        int pivot;
-        for (int i = 0; i < d; ++i) {
-            pivot = left + (right - left) / 2;
+
+        for (int i = 0; i < d; i++) {
+            int pivot = left + (right - left) / 2;
             if (idx <= pivot) {
                 node = node.left;
                 right = pivot;
@@ -55,9 +55,8 @@ class Solution {
         // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
         // Perform binary search to check how many nodes exist.
         int left = 1, right = (int) Math.pow(2, d) - 1;
-        int pivot;
         while (left <= right) {
-            pivot = left + (right - left) / 2;
+            int pivot = left + (right - left) / 2;
             if (exists(pivot, d, root))
                 left = pivot + 1;
             else
