4dca82e7f09f8075efa271b8d2759d748b694d70
Jerome <nietsemorej@gmail.com>
2021-11-02 10:07:43 -0400

    Add comments

--- 980.unique-paths-iii.java
+++ 980.unique-paths-iii.java
@@ -7,11 +7,32 @@
 // @lc code=start
 
 class Solution {
-    // 1.
-
-    int rows, cols;
+    int rows, cols, path_count;
     int[][] grid;
-    int path_count;
+
+    public int uniquePathsIII(int[][] grid) {
+        int non_obstacles = 0, start_row = 0, start_col = 0;
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        this.path_count = 0;
+        this.grid = grid;
+        // step 1). initialize the conditions for backtracking
+        // i.e. initial state and final state
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (grid[row][col] >= 0)
+                    non_obstacles += 1;
+                if (grid[row][col] == 1) {
+                    start_row = row;
+                    start_col = col;
+                }
+            }
+        }
+
+        backtrack(start_row, start_col, non_obstacles);
+
+        return this.path_count;
+    }
 
     public void backtrack(int row, int col, int remain) {
         // base case for the termination of backtracking
@@ -48,32 +69,5 @@ class Solution {
         grid[row][col] = temp;
     }
 
-    public int uniquePathsIII(int[][] grid) {
-        int non_obstacles = 0, start_row = 0, start_col = 0;
-
-        this.rows = grid.length;
-        this.cols = grid[0].length;
-
-        // step 1). initialize the conditions for backtracking
-        // i.e. initial state and final state
-        for (int row = 0; row < rows; ++row)
-            for (int col = 0; col < cols; ++col) {
-                int cell = grid[row][col];
-                if (cell >= 0)
-                    non_obstacles += 1;
-                if (cell == 1) {
-                    start_row = row;
-                    start_col = col;
-                }
-            }
-
-        this.path_count = 0;
-        this.grid = grid;
-
-        // kick-off the backtracking
-        backtrack(start_row, start_col, non_obstacles);
-
-        return this.path_count;
-    }
 }
 // @lc code=end
