ce02e8a202473ab3856c1acb759d7c09d2239b3b
Jerome <nietsemorej@gmail.com>
2021-11-03 19:04:07 -0400

    Add comments

--- 973.k-closest-points-to-origin.java
+++ 973.k-closest-points-to-origin.java
@@ -31,10 +31,17 @@ import java.util.PriorityQueue;
 //     }
 // }
 
+// Average case: pivot is generally near the mid of l and r; 
+// therefore we have n + (n/2 + n/4 +...) <= n + (n) = 2 * n = O(n).
+// As for worst case: K = n, and points is already sorted by the distance to origin. 
+// @Frimish's code will cost n - 1 + ... + 1 = (n - 1) * n / 2 = O(n^2) time.
 class Solution {
+    // since return the answer in any order
+    // we can consider quick sort (quick select)
     public int[][] kClosest(int[][] points, int K) {
         int len = points.length, l = 0, r = len - 1;
         while (l <= r) {
+            // one round quick select
             int mid = helper(points, l, r);
             if (mid == K)
                 break;
@@ -48,13 +55,17 @@ class Solution {
     }
 
     private int helper(int[][] A, int l, int r) {
+        // pick A[l] as pivot,
+        // let every e <= pivot on the left side of pivot and e > pivot on the right
         int[] pivot = A[l];
         while (l < r) {
-            while (l < r && compare(A[r], pivot) >= 0)
+            while (l < r && compare(A[r], pivot) >= 0) {
                 r--;
+            }
             A[l] = A[r];
-            while (l < r && compare(A[l], pivot) <= 0)
+            while (l < r && compare(A[l], pivot) <= 0) {
                 l++;
+            }
             A[r] = A[l];
         }
         A[l] = pivot;
