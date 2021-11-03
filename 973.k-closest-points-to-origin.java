00886d51cfdca16955272d0a9d9c71361a99ba79
Jerome <nietsemorej@gmail.com>
2021-11-03 18:41:52 -0400

    PQ and quick sort, which one is better?

--- 973.k-closest-points-to-origin.java
+++ 973.k-closest-points-to-origin.java
@@ -12,14 +12,8 @@ import java.util.PriorityQueue;
 
 // class Solution {
 //     public int[][] kClosest(int[][] points, int k) {
-//         int[][] res = new int[k][];
-//         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-//             public int compare(int[] p1, int[] p2) {
-//                 int dis1 = p1[0] * p1[0] + p1[1] * p1[1];
-//                 int dis2 = p2[0] * p2[0] + p2[1] * p2[1];
-//                 return dis2 - dis1;
-//             }
-//         });
+//         PriorityQueue<int[]> pq = new PriorityQueue<int[]>(
+//                 (p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
 
 //         for (int i = 0; i < points.length; i++) {
 //             pq.add(points[i]);
@@ -28,6 +22,7 @@ import java.util.PriorityQueue;
 //             }
 //         }
 
+//         int[][] res = new int[k][];
 //         for (int i = 0; i < k; i++) {
 //             res[i] = pq.poll();
 //         }
@@ -37,50 +32,37 @@ import java.util.PriorityQueue;
 // }
 
 class Solution {
-    Random rand = new Random();
-
-    public int[][] kClosest(int[][] points, int k) {
-        int n = points.length;
-
-        // You may return the answer in any order
-        // So we can use quick sort method for reference
-        random_select(points, 0, n - 1, k);
-        return Arrays.copyOfRange(points, 0, k);
-    }
-
-    public void random_select(int[][] points, int left, int right, int k) {
-        int pivotId = left + rand.nextInt(right - left + 1);
-        int pivotDist = points[pivotId][0] * points[pivotId][0] + points[pivotId][1] * points[pivotId][1];
-        // swap pivot to the rightmost
-        swap(points, right, pivotId);
-
-        int i = left;
-        for (int j = left; j < right; j++) {
-            int jDist = points[j][0] * points[j][0] + points[j][1] * points[j][1];
-            if (jDist <= pivotDist) {
-                swap(points, i, j);
-                i++;
+    public int[][] kClosest(int[][] points, int K) {
+        int len = points.length, l = 0, r = len - 1;
+        while (l <= r) {
+            int mid = helper(points, l, r);
+            if (mid == K)
+                break;
+            if (mid < K) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
-
-        // swap pivot back to i
-        swap(points, i, right);
-
-        // elements in [left, i-1] <= pivot, in [i+1, right] > pivot
-        int notGreaterCNT = i - left + 1;
-        if (k < notGreaterCNT) {
-            random_select(points, left, i - 1, k);
-        } else if (k > notGreaterCNT) {
-            random_select(points, i + 1, right, k - notGreaterCNT);
-        } else {
-            return;
-        }
+        return Arrays.copyOfRange(points, 0, K);
     }
 
-    public void swap(int[][] points, int index1, int index2) {
-        int[] temp = points[index1];
-        points[index1] = points[index2];
-        points[index2] = temp;
+    private int helper(int[][] A, int l, int r) {
+        int[] pivot = A[l];
+        while (l < r) {
+            while (l < r && compare(A[r], pivot) >= 0)
+                r--;
+            A[l] = A[r];
+            while (l < r && compare(A[l], pivot) <= 0)
+                l++;
+            A[r] = A[l];
+        }
+        A[l] = pivot;
+        return l;
+    }
+
+    private int compare(int[] p1, int[] p2) {
+        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
     }
 }
 
