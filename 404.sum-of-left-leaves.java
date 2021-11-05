fdc0eddb6b0083fd290ffbf11321bb493845b6fb
Jerome <nietsemorej@gmail.com>
2021-11-05 12:51:36 -0400

    god.. morris...

--- 404.sum-of-left-leaves.java
+++ 404.sum-of-left-leaves.java
@@ -11,31 +11,72 @@
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
+// class Solution {
+// int sum;
+
+// public int sumOfLeftLeaves(TreeNode root) {
+// // 1. walk through all the nodes, find the left leaves O(n)
+// // how to identify the left leace
+// // check left: dfs(node, left) or dfs (node, right)
+// // check leaves: if (left==null&&riget==null)
+// // 2. add them
+// sum = 0;
+// dfs(root, false);
+// return sum;
+// }
+
+// public void dfs(TreeNode root, boolean isLeft) {
+// if (isLeft && root.left == null && root.right == null) {
+// sum += root.val;
+// }
+
+// if (root.left != null) {
+// dfs(root.left, true);
+// }
+// if (root.right != null) {
+// dfs(root.right, false);
+// }
+// }
+// }
+
 class Solution {
-    int sum;
-
     public int sumOfLeftLeaves(TreeNode root) {
-        // 1. walk through all the nodes, find the left leaves O(n)
-        // how to identify the left leace
-        // check left: dfs(node, left) or dfs (node, right)
-        // check leaves: if (left==null&&riget==null)
-        // 2. add them
-        sum = 0;
-        dfs(root, false);
-        return sum;
-    }
-
-    public void dfs(TreeNode root, boolean isLeft) {
-        if (isLeft && root.left == null && root.right == null) {
-            sum += root.val;
+        int totalSum = 0;
+        TreeNode currentNode = root;
+        while (currentNode != null) {
+            // If there is no left child, we can simply explore the right subtree
+            // without needing to worry about keeping track of currentNode's other
+            // child.
+            if (currentNode.left == null) {
+                currentNode = currentNode.right;
+            } else {
+                TreeNode previous = currentNode.left;
+                // Check if this left node is a leaf node.
+                if (previous.left == null && previous.right == null) {
+                    totalSum += previous.val;
+                }
+                // Find the inorder predecessor for currentNode.
+                while (previous.right != null && !previous.right.equals(currentNode)) {
+                    previous = previous.right;
+                }
+                // We've not yet visited the inorder predecessor. This means that we
+                // still need to explore currentNode's left subtree. Before doing this,
+                // we will put a link back so that we can get back to the right subtree
+                // when we need to.
+                if (previous.right == null) {
+                    previous.right = currentNode;
+                    currentNode = currentNode.left;
+                }
+                // We have already visited the inorder predecessor. This means that we
+                // need to remove the link we added, and then move onto the right
+                // subtree and explore it.
+                else {
+                    previous.right = null;
+                    currentNode = currentNode.right;
+                }
+            }
         }
-
-        if (root.left != null) {
-            dfs(root.left, true);
-        }
-        if (root.right != null) {
-            dfs(root.right, false);
-        }
+        return totalSum;
     }
 }
 // @lc code=end
