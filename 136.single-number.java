9ec8ba2024fc819bc1fcfe2317753428e7433086
Jerome <nietsemorej@gmail.com>
2021-11-06 09:54:39 -0400

    much more understandable approach, 8ms, 35%, 17%.

--- 136.single-number.java
+++ 136.single-number.java
@@ -6,13 +6,31 @@
 
 // @lc code=start
 
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int single = 0;
+//         for (int num : nums) {
+//             single ^= num;
+//         }
+//         return single;
+//     }
+// }
+
 class Solution {
     public int singleNumber(int[] nums) {
-        int single = 0;
-        for (int num : nums) {
-            single ^= num;
+        int ans = 0;
+        // only once and 3 times,
+        // we can count the 1s in each bit
+        for (int i = 0; i < 32; i++) {
+            int ones = 0;
+            for (int n : nums) {
+                ones += ((n >> i) & 1);
+            }
+            if (ones % 3 != 0) {
+                ans = ans + (1 << i);
+            }
         }
-        return single;
+        return ans;
     }
 }
 // @lc code=end
