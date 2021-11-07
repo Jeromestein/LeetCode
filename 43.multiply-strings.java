3646a5a6f0db09d47ab3d4340905346476c1dc74
Jerome <nietsemorej@gmail.com>
2021-11-07 11:30:12 -0500

    Add comments

--- 43.multiply-strings.java
+++ 43.multiply-strings.java
@@ -5,26 +5,32 @@
  */
 
 // @lc code=start
+
 class Solution {
     public String multiply(String num1, String num2) {
+        // for each digits in nums, do the mul with num2
+        // 1. from right to left, num1[i]*num2[j]=mul={cin, cout}
+        // 2. out[i+j]+=cin, out[i+j-1]+=cout;
         int m = num1.length(), n = num2.length();
-        int[] pos = new int[m + n];
+        int[] out = new int[m + n];
 
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
                 int p1 = i + j, p2 = i + j + 1;
-                int sum = mul + pos[p2];
+                int sum = mul + out[p2];
 
-                pos[p1] += sum / 10;
-                pos[p2] = (sum) % 10;
+                out[p1] += sum / 10;
+                out[p2] = (sum) % 10;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int p : pos)
+        for (int p : out)
+            // no leading zeros
             if (sb.length() != 0 || p != 0)
                 sb.append(p);
+
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
