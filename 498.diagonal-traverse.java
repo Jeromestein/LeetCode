6a9aae723e73f1339ed7c72a10b20afe08cd9f89
Jerome <nietsemorej@gmail.com>
2021-11-10 20:49:42 -0500

    Add comments

--- 498.diagonal-traverse.java
+++ 498.diagonal-traverse.java
@@ -12,7 +12,7 @@ class Solution {
             return new int[0];
         }
 
-        int N = matrix.length, M = matrix[0].length;
+        int m = matrix.length, n = matrix[0].length;
 
         // Incides that will help us progress through
         // the matrix, one element at a time.
@@ -23,22 +23,22 @@ class Solution {
         boolean isUP = true;
 
         // The final result array
-        int[] res = new int[N * M];
+        int[] res = new int[m * n];
         int index = 0;
 
-        while (row < N && column < M) {
+        while (row < m && column < n) {
             res[index++] = matrix[row][column];
 
             int new_row = row + (isUP ? -1 : 1);
             int new_column = column + (isUP ? 1 : -1);
 
-            if (new_row < 0 || new_row == N || new_column < 0 || new_column == M) {
+            if (new_row < 0 || new_row == m || new_column < 0 || new_column == n) {
                 if (isUP) {
                     // For an upwards going diagonal having [i, j] as its tail
                     // If [i, j + 1] is within bounds, then it becomes
                     // the next head. Otherwise, the element directly below
                     // i.e. the element [i + 1, j] becomes the next head
-                    if (column < M - 1) {
+                    if (column < n - 1) {
                         column += 1;
                     } else {
                         row += 1;
@@ -48,7 +48,7 @@ class Solution {
                     // if [i + 1, j] is within bounds, then it becomes
                     // the next head. Otherwise, the element directly below
                     // i.e. the element [i, j + 1] becomes the next head
-                    if (row < N - 1) {
+                    if (row < m - 1) {
                         row += 1;
                     } else {
                         column += 1;
