0299fe2847485a2bb7b18c1784a692e5985b2c79
Jerome <nietsemorej@gmail.com>
2021-11-13 22:54:51 -0500

    Make it readable

--- 253.meeting-rooms-ii.java
+++ 253.meeting-rooms-ii.java
@@ -14,18 +14,10 @@ class Solution {
         }
 
         // Min heap
-        PriorityQueue<Integer> allocator = new PriorityQueue<Integer>(intervals.length, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                return a - b;
-            }
-        });
+        PriorityQueue<Integer> allocator = new PriorityQueue<>();
 
         // Sort the intervals by start time
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(final int[] a, final int[] b) {
-                return a[0] - b[0];
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         // Add the first meeting, finishing time
         allocator.add(intervals[0][1]);
