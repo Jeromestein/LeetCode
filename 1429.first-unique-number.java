90affa6f8d354b315f9e48ff0068aa223eefe840
Jeromestein <nietsemorej@gmail.com>
2021-11-28 16:02:22 -0500

    Add comments

--- 1429.first-unique-number.java
+++ 1429.first-unique-number.java
@@ -18,22 +18,18 @@ class FirstUnique {
     }
 
     public int showFirstUnique() {
-        // If the queue contains values, we need to get the first one from it.
-        // We can do this by making an iterator, and getting its first item.
+
         if (!setQueue.isEmpty()) {
             return setQueue.iterator().next();
+        } else {
+            return -1;
         }
-        return -1;
     }
 
     public void add(int value) {
-        // Case 1: This value is not yet in the data structure.
-        // It should be ADDED.
         if (!isUnique.containsKey(value)) {
             isUnique.put(value, true);
             setQueue.add(value);
-            // Case 2: This value has been seen once, so is now becoming
-            // non-unique. It should be REMOVED.
         } else if (isUnique.get(value)) {
             isUnique.put(value, false);
             setQueue.remove(value);
