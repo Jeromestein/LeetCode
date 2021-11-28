3f062615d968961f800bb409459c3c22af9cbee3
Jeromestein <nietsemorej@gmail.com>
2021-11-28 15:19:43 -0500

    Add comments

--- 1263.minimum-moves-to-move-a-box-to-their-target-location.java
+++ 1263.minimum-moves-to-move-a-box-to-their-target-location.java
@@ -6,55 +6,58 @@
 
 // @lc code=start
 class Solution {
-    char[][] g;
+    // Time : O(n^2) ?
+    // Space: O(m*n) ?
+    char[][] grid;
     int m, n;
-    int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } }; // right, down, left, up
 
     public int minPushBox(char[][] grid) {
-        g = grid;
-        m = g.length;
-        n = g[0].length;
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        int step = 0;
+        boolean[][][] visited = new boolean[m][n][4]; // considering 4 directons
 
-        // 2D is not enough, still need one more dimension to represent four direction
-        boolean[][][] visit = new boolean[m][n][4];
-
-        Queue<int[]> q = new LinkedList<>();
-        int[] st = new int[] { -1, -1 }, target = new int[] { -1, -1 }, pl = new int[] { -1, -1 };
+        Queue<int[]> boxQ = new LinkedList<>();
+        Queue<int[]> playerQ = new LinkedList<>();
+        int[] boxLoc = new int[2], targetLoc = new int[2], playerLoc = new int[2];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (g[i][j] == 'B')
-                    st = new int[] { i, j };
-                if (g[i][j] == 'T')
-                    target = new int[] { i, j };
-                if (g[i][j] == 'S')
-                    pl = new int[] { i, j };
+                if (grid[i][j] == 'B')
+                    boxLoc = new int[] { i, j };
+                if (grid[i][j] == 'T')
+                    targetLoc = new int[] { i, j };
+                if (grid[i][j] == 'S')
+                    playerLoc = new int[] { i, j };
             }
         }
-        // alwasys add the current box coordinate and the player current coordinate
-        q.offer(new int[] { st[0], st[1], pl[0], pl[1] });
-        int step = 0;
-        while (!q.isEmpty()) {
-            for (int i = 0, l = q.size(); i < l; i++) {
-                int[] curr = q.poll();
-                // get to the target
-                if (curr[0] == target[0] && curr[1] == target[1])
-                    return step;
-                for (int k = 0; k < 4; k++) {
-                    if (visit[curr[0]][curr[1]][k])
+        boxQ.offer(new int[] { boxLoc[0], boxLoc[1] });
+        playerQ.offer(new int[] { playerLoc[0], playerLoc[1] });
+
+        while (!boxQ.isEmpty()) {
+            for (int i = 0, l = boxQ.size(); i < l; i++) { // as we care about all directions, it should be like
+                                                           // this.--> it's related to calculating 'step'
+                int[] currBoxLoc = boxQ.poll();
+                int[] currPlayerLoc = playerQ.poll();
+                if (currBoxLoc[0] == targetLoc[0] && currBoxLoc[1] == targetLoc[1])
+                    return step; // If box arrives at the target, it returns 'step'
+                for (int j = 0; j < dir.length; j++) { // Checking all directions
+                    if (visited[currBoxLoc[0]][currBoxLoc[1]][j])
                         continue;
-                    // where pl stands, have room to push;
-                    int r0 = curr[0] + dir[k][0], c0 = curr[1] + dir[k][1];
-                    if (r0 < 0 || r0 >= m || c0 < 0 || c0 >= n || g[r0][c0] == '#')
-                        continue;
-                    // box next spots;
-                    int nextr = curr[0] - dir[k][0], nextc = curr[1] - dir[k][1];
-                    if (nextr < 0 || nextr >= m || nextc < 0 || nextc >= n || g[nextr][nextc] == '#')
-                        continue;
-                    if (!reachable(r0, c0, curr))
-                        continue;
-                    visit[curr[0]][curr[1]][k] = true;
-                    int plr = curr[0], plc = curr[1];
-                    q.offer(new int[] { nextr, nextc, plr, plc });
+                    int[] d = dir[j];
+                    int r0 = currBoxLoc[0] + d[0], c0 = currBoxLoc[1] + d[1]; // where player stands, need a space to
+                                                                              // push
+                    if (r0 < 0 || r0 >= m || c0 < 0 || c0 >= n || grid[r0][c0] == '#')
+                        continue; // if no space, ignore(/continue)
+                    int r = currBoxLoc[0] - d[0], c = currBoxLoc[1] - d[1]; // the box location after pushed
+                    if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == '#')
+                        continue; // if no space for box, ignore(/continue)
+                    if (!reachable(r0, c0, currBoxLoc, currPlayerLoc))
+                        continue; // Check if the player can reach (r0, c0). if not, continue
+                    visited[currBoxLoc[0]][currBoxLoc[1]][j] = true; // After pushed, the player is at 'currBoxLoc'.
+                    boxQ.offer(new int[] { r, c }); // update queues accordingly.
+                    playerQ.offer(new int[] { currBoxLoc[0], currBoxLoc[1] });
                 }
             }
             step++;
@@ -62,26 +65,21 @@ class Solution {
         return -1;
     }
 
-    /**
-     * 1. has room to push the box; 2. has a way to go to the room to push the box.
-     * Therefore you need another bfs to find if the path exist;
-     */
-    private boolean reachable(int i, int j, int[] curr) {
+    private boolean reachable(int i, int j, int[] boxLoc, int[] playerLoc) {
+        // (i,j) is a location where the play will push the box.
         Queue<int[]> q = new LinkedList<>();
-        // start from the player's current place
-        q.offer(new int[] { curr[2], curr[3] });
-        boolean[][] visit = new boolean[m][n];
-        // set the box position as a block
-        visit[curr[0]][curr[1]] = true;
+        q.offer(playerLoc);
+        boolean[][] visited = new boolean[m][n];
+        visited[boxLoc[0]][boxLoc[1]] = true; // player cannot go through the spot where the box is located at.
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            if (cur[0] == i && cur[1] == j)
+            int[] currPlLoc = q.poll();
+            if (currPlLoc[0] == i && currPlLoc[1] == j)
                 return true;
             for (int[] d : dir) {
-                int r = cur[0] - d[0], c = cur[1] - d[1]; // box next spots;
-                if (r < 0 || r >= m || c < 0 || c >= n || visit[r][c] || g[r][c] == '#')
-                    continue;
-                visit[r][c] = true;
+                int r = currPlLoc[0] + d[0], c = currPlLoc[1] + d[1]; // player's location after moving
+                if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c] || grid[r][c] == '#')
+                    continue; // check if player can move to (r,c)
+                visited[r][c] = true; // if possible, check it visited.
                 q.offer(new int[] { r, c });
             }
         }
