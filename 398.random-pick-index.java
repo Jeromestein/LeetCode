3d99668e5c47fccc8cbbde784c2ea48510db3ec2
Jeromestein <nietsemorej@gmail.com>
2021-12-01 15:40:23 -0500

    45ms, 99%, 99%. Reservoir sampling

--- 398.random-pick-index.java
+++ 398.random-pick-index.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /*
  * @lc app=leetcode id=398 lang=java
  *
@@ -6,20 +8,51 @@
 
 // @lc code=start
 
+// class Solution {
+//     Map<Integer, List<Integer>> map;
+
+//     public Solution(int[] nums) {
+//         map = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             map.computeIfAbsent(nums[i], v -> new ArrayList<>()).add(i);
+//         }
+//     }
+
+//     public int pick(int target) {
+//         int n = map.get(target).size();
+//         int r = (int) (Math.random() * n);
+//         return map.get(target).get(r);
+//     }
+// }
 class Solution {
-    Map<Integer, List<Integer>> map;
+    int[] nums;
+    Random rand;
 
     public Solution(int[] nums) {
-        map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.computeIfAbsent(nums[i], v -> new ArrayList<>()).add(i);
-        }
+        this.nums = nums;
+        this.rand = new Random();
     }
 
     public int pick(int target) {
-        int n = map.get(target).size();
-        int r = (int) (Math.random() * n);
-        return map.get(target).get(r);
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            // if nums[i] is equal to target, i is a potential candidate
+            // which needs to be chosen uniformly at random
+            if (this.nums[i] == target) {
+                // increment the count of total candidates
+                // available to be chosen uniformly at random
+                count++;
+                // we pick the current number with probability 1 / count (reservoir sampling)
+                // Returns a pseudorandom, uniformly distributed int value between 0 (inclusive)
+                // and the specified value (exclusive)
+                if (rand.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
     }
 }
 
