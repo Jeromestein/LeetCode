172d59a8df8bbede5c0b0502502743cdb3de7bc1
Jerome <nietsemorej@gmail.com>
2021-12-25 09:11:54 -0500

    Add comments

--- 398.random-pick-index.java
+++ 398.random-pick-index.java
@@ -6,21 +6,52 @@
 
 // @lc code=start
 
+// class Solution {
+
+//     Map<Integer, List<Integer>> map;
+
+//     public Solution(int[] nums) {
+//         map = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             map.computeIfAbsent(nums[i], v -> new ArrayList<>()).add(i);
+//         }
+//     }
+
+//     public int pick(int target) {
+//         int n = map.get(target).size();
+//         int r = (int) (Math.random() * n);
+//         return map.get(target).get(r);
+//     }
+// }
+
 class Solution {
 
-    Map<Integer, List<Integer>> map;
+    private int[] nums;
+    private Random rand;
 
     public Solution(int[] nums) {
-        map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.computeIfAbsent(nums[i], v -> new ArrayList<>()).add(i);
-        }
+        this.nums = nums;
+        this.rand = new Random();
     }
 
     public int pick(int target) {
-        int n = map.get(target).size();
-        int r = (int) (Math.random() * n);
-        return map.get(target).get(r);
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            // if nums[i] is equal to target, i is a potential candidate
+            // which needs to be chosen uniformly at random
+            if (this.nums[i] == target) {
+                // increment the count of total candidates
+                // available to be chosen uniformly at random
+                count++;
+                // we pick the current number with probability 1 / count (reservoir sampling)
+                if (rand.nextInt(count) == 0) {
+                    idx = i;
+                }
+            }
+        }
+        return idx;
     }
 }
 
