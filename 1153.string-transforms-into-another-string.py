1e047706dbfc55db2cd47ec88e77c04a9108734e
Jerome <nietsemorej@gmail.com>
2021-12-27 10:43:26 -0500

    32ms, 56%, 83%.

---
+++ 1153.string-transforms-into-another-string.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=1153 lang=python3
+#
+# [1153] String Transforms Into Another String
+#
+
+# @lc code=start
+class Solution:
+    def canConvert(self, str1: str, str2: str) -> bool:
+        if str1 == str2:
+            return True
+
+        convertMap = dict()
+        letterSet = set()
+
+        for char1, char2 in zip(str1, str2):
+            if char1 not in convertMap:
+                convertMap[char1] = char2
+                letterSet.add(char2)
+            elif convertMap[char1] != char2:
+                return False
+
+        return len(letterSet) < 26
+
+
+# @lc code=end
