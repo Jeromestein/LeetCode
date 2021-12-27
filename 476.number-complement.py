c7e99914560a99227dead337924d571c1f6fa4f1
Jerome <nietsemorej@gmail.com>
2021-12-27 10:18:50 -0500

    Built-in Functions to Construct 1-bits Bitmask

--- 476.number-complement.py
+++ 476.number-complement.py
@@ -6,16 +6,20 @@
 
 # @lc code=start
 
+# class Solution:
+#     def findComplement(self, num: int) -> int:
+#         res = 0
+#         exp = 0
+#         while num > 0:
+#             if num % 2 == 0:
+#                 res += 2**exp
+#             exp += 1
+#             num = num//2
+
+#         return res
+
+# Built-in Functions to Construct 1-bits Bitmask
 class Solution:
-    def findComplement(self, num: int) -> int:
-        res = 0
-        exp = 0
-        while num > 0:
-            if num % 2 == 0:
-                res += 2**exp
-            exp += 1
-            num = num//2
-
-        return res
-
+    def findComplement(self, num):
+        return (1 << num.bit_length()) - num - 1
 # @lc code=end
