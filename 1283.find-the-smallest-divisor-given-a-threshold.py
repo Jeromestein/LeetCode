ca1c0045995ec7c0df35900560173ca2689c6541
Jerome <nietsemorej@gmail.com>
2021-12-29 16:39:23 -0500

    make it clean and readable

--- 1283.find-the-smallest-divisor-given-a-threshold.py
+++ 1283.find-the-smallest-divisor-given-a-threshold.py
@@ -11,10 +11,13 @@ class Solution:
         l, r = 1, max(nums)
         while l < r:
             m = (l + r) // 2
-            if sum((i + m - 1) // m for i in nums) > threshold:
-                l = m + 1
-            else:
+            sum = 0
+            for num in nums:
+                sum += math.ceil(num/m)
+            if sum <= threshold:
                 r = m
+            else:
+                l = m + 1
         return l
 
 
