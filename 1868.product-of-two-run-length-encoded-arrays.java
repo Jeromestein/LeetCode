bb0d10904faec3688063a1a6b53308433e331b4e
Jeromestein <nietsemorej@gmail.com>
2021-12-30 21:41:54 -0500

    Add comments

--- 1868.product-of-two-run-length-encoded-arrays.java
+++ 1868.product-of-two-run-length-encoded-arrays.java
@@ -14,11 +14,12 @@ class Solution {
         while (p1 < encoded1.length) {
             int len = Math.min(encoded1[p1][1], encoded2[p2][1]);
             int mult = encoded1[p1][0] * encoded2[p2][0];
+            // to handle cases like [[1,3],[2,3]] *[[6,3],[3,3]] --> [[6,6]]
+            if (res.size() > 0 && res.get(res.size() - 1).get(0) == mult)
+                // update previous mult in res
+                // instead of adding a new one
+                res.get(res.size() - 1).set(1, res.get(res.size() - 1).get(1) + len);
 
-            if (res.size() > 0 && res.get(res.size() - 1).get(0) == mult) // to handle cases like [[1,3],[2,3]] *
-                                                                          // [[6,3],[3,3]] --> [[6,6]]
-                res.get(res.size() - 1).set(1, res.get(res.size() - 1).get(1) + len); // update previous mult in res
-                                                                                      // instead of adding a new one
             else
                 res.add(Arrays.asList(mult, len));
 
