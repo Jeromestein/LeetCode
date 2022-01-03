a1c1e3637169a1fe8cf7b4aa39559397bf63184c
Jeromestein <nietsemorej@gmail.com>
2022-01-02 23:58:18 -0500

    731ms, 67%, 65%.

---
+++ 997.find-the-town-judge.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=997 lang=python3
+#
+# [997] Find the Town Judge
+#
+
+# @lc code=start
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if len(trust) < n - 1:
+            return -1
+
+        trust_scores = [0] * (n + 1)
+
+        for a, b in trust:
+            trust_scores[a] -= 1
+            trust_scores[b] += 1
+
+        for i, score in enumerate(trust_scores[1:], 1):
+            if score == n - 1:
+                return i
+        return -1
+
+
+# @lc code=end
