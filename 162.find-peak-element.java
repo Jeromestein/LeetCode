a1481a681487d38d2f3d8821960d6ac88d7faf51
Jerome <nietsemorej@gmail.com>
2022-01-11 12:38:24 -0500

    Much cleaner code

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -8,45 +8,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length - 1;
-        while (left <= right) {
+        // nums[-1] = nums[n] = -∞ guarantees at least one peak
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (leftDerivative(nums, mid) == 1 && rightDerivative(nums, mid) == -1) {
-                return mid;
-            } else if (leftDerivative(nums, mid) == 1 && rightDerivative(nums, mid) == 1) {
+            if (nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
-        return -1;
-    }
-
-    // 1 : +
-    // -1: -
-    // 0 : 0
-    // nums[i] != nums[i + 1]
-    // nums[-1] = nums[n] = -∞.
-    public int leftDerivative(int[] nums, int index) {
-        if (index == 0) {
-            return 1;
-        }
-
-        if (nums[index] > nums[index - 1]) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }
-
-    public int rightDerivative(int[] nums, int index) {
-        if (index == nums.length - 1) {
-            return -1;
-        }
-        if (nums[index] > nums[index + 1]) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return left;
     }
 }
 // @lc code=end
