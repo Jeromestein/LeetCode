4b8573d9769e2715c2c17fb84c884c5b50c9ef10
Jerome <nietsemorej@gmail.com>
2022-01-13 13:01:04 -0500

    Add comments

--- 302.smallest-rectangle-enclosing-black-pixels.py
+++ 302.smallest-rectangle-enclosing-black-pixels.py
@@ -1,5 +1,5 @@
 #
-# @lc app=leetcode id=302 lang=python
+# @lc app=leetcode id=302 lang=python3
 #
 # [302] Smallest Rectangle Enclosing Black Pixels
 #
@@ -8,16 +8,21 @@
 
 class Solution(object):
     def minArea(self, image, x, y):
+        m, n = len(image), len(image[0])
+        # binary search [0, x) to find top
         top = self.searchRows(image, 0, x, True)
-        bottom = self.searchRows(image, x + 1, len(image), False)
+        # [x+1, m)
+        bottom = self.searchRows(image, x + 1, m, False)
+        # [0, y)
         left = self.searchColumns(image, 0, y, top, bottom, True)
-        right = self.searchColumns(
-            image, y + 1, len(image[0]), top, bottom, False)
+        # [y+1, n)
+        right = self.searchColumns(image, y + 1, n, top, bottom, False)
         return (right - left) * (bottom - top)
 
     def searchRows(self, image, i, j, opt):
         while i != j:
-            m = (i + j) / 2
+            m = (i + j) // 2
+            # find '1' in string image[m]
             if ('1' in image[m]) == opt:
                 j = m
             else:
@@ -26,8 +31,8 @@ class Solution(object):
 
     def searchColumns(self, image, i, j, top, bottom, opt):
         while i != j:
-            m = (i + j) / 2
-            if any(image[k][m] == '1' for k in xrange(top, bottom)) == opt:
+            m = (i + j) // 2
+            if any(image[k][m] == '1' for k in range(top, bottom)) == opt:
                 j = m
             else:
                 i = m + 1
