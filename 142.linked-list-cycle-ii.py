d5bd79a1170bf201e98bde8f844a4eeb233900b8
Jerome <nietsemorej@gmail.com>
2022-01-19 13:11:42 -0500

    make it clean

--- 142.linked-list-cycle-ii.py
+++ 142.linked-list-cycle-ii.py
@@ -11,42 +11,18 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def getIntersect(self, head):
-        tortoise = head
-        hare = head
-
-        # A fast pointer will either loop around a cycle and meet the slow
-        # pointer or reach the `null` at the end of a non-cyclic list.
-        while hare is not None and hare.next is not None:
-            tortoise = tortoise.next
-            hare = hare.next.next
-            if tortoise == hare:
-                return tortoise
-
-        return None
-
-    def detectCycle(self, head):
-        if head is None:
-            return None
-
-        # If there is a cycle, the fast/slow pointers will intersect at some
-        # node. Otherwise, there is no cycle, so we cannot find an entrance to
-        # a cycle.
-        intersect = self.getIntersect(head)
-        if intersect is None:
-            return None
-
-        # To find the entrance to the cycle, we have two pointers traverse at
-        # the same speed -- one from the front of the list, and the other from
-        # the point of intersection.
-        ptr1 = head
-        ptr2 = intersect
-        while ptr1 != ptr2:
-            ptr1 = ptr1.next
-            ptr2 = ptr2.next
-
-        return ptr1
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None  # if not (fast and fast.next): return None
+        while head != slow:
+            head, slow = head.next, slow.next
+        return head
 
 
 # @lc code=end
