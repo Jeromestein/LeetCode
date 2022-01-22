e5ae105083f8b4d986cd796d5c65bf7e87921eca
Jerome <nietsemorej@gmail.com>
2022-01-22 11:18:27 -0500

    Add comments

--- 1510.stone-game-iv.py
+++ 1510.stone-game-iv.py
@@ -11,6 +11,7 @@ class Solution:
     def winnerSquareGame(self, n: int) -> bool:
         # Alice starting first
         dp = [False]*(n+1)
+        # 1<=i<n+1
         for i in range(1, n+1):
             for k in range(1, int(i**0.5)+1):
                 if dp[i-k*k] == False:
