91e1388ab613ff010e3f54c11c42138b1240c6ca
Jerome <nietsemorej@gmail.com>
2022-09-02 12:39:28 -0400

    22ms, 98%, 32%.

--- 2272.substring-with-largest-variance.java
+++ 2272.substring-with-largest-variance.java
@@ -5,77 +5,77 @@
  */
 
 // @lc code=start
-// class Solution {
-//     public int largestVariance(String s) {
-//         int c, i, j;
-//         int res = 0;
-//         int[][][] dp = new int[26][26][2];
-//         for (i = 0; i < 26; i++)
-//             for (j = 0; j < 26; j++)
-//                 dp[i][j][1] = -10000;
-
-//         for (int idx = 0; idx < s.length(); idx++) {
-//             c = s.charAt(idx) - 'a';
-//             for (i = 0; i < 26; i++) {
-//                 if (c != i) {
-//                     dp[c][i][0] += 1;
-//                     if (dp[c][i][1] >= -1)
-//                         dp[c][i][1] += 1;
-
-//                     dp[i][c][1] = dp[i][c][0] - 1;
-//                     if (dp[i][c][0] > 0)
-//                         dp[i][c][0] -= 1;
-
-//                     if (dp[i][c][1] > res)
-//                         res = dp[i][c][1];
-//                     if (dp[c][i][1] > res)
-//                         res = dp[c][i][1];
-//                 }
-//             }
-//         }
-
-//         return res;
-//     }
-// }
-
 class Solution {
     public int largestVariance(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++)
-            freq[(int) (s.charAt(i) - 'a')]++;
+        int res = 0;
+        // dp[a][b][0] is the freq of a
+        // dp[a][b][1] is the max(freqa - freqb) in all substring, minmum is -1
+        int[][][] dp = new int[26][26][2];
+        for (int i = 0; i < 26; i++)
+            for (int j = 0; j < 26; j++)
+                dp[i][j][1] = -10000;
 
-        int maxVariance = 0;
-        for (int a = 0; a < 26; a++) {
-            for (int b = 0; b < 26; b++) {
-                int remainingA = freq[a];
-                int remainingB = freq[b];
-                if (a == b || remainingA == 0 || remainingB == 0)
-                    continue;
+        for (int idx = 0; idx < s.length(); idx++) {
+            int i = s.charAt(idx) - 'a';
+            for (int j = 0; j < 26; j++) {
+                if (i != j) {
+                    // current ch freq + 1
+                    dp[i][j][0] += 1;
+                    dp[i][j][1] += 1;
 
-                // run Kadanes Algorithm on each possible character pairs (A & B)
-                int currBFreq = 0, currAFreq = 0;
-                for (int i = 0; i < s.length(); i++) {
-                    int c = (int) (s.charAt(i) - 'a');
+                    // update b&a situation
+                    dp[j][i][1] = dp[j][i][0] - 1;
+                    if (dp[j][i][0] > 0)
+                        dp[j][i][0] -= 1;
 
-                    if (c == b)
-                        currBFreq++;
-                    if (c == a) {
-                        currAFreq++;
-                        remainingA--;
-                    }
-
-                    if (currAFreq > 0)
-                        maxVariance = Math.max(maxVariance, currBFreq - currAFreq);
-
-                    if (currBFreq < currAFreq && remainingA >= 1) {
-                        currBFreq = 0;
-                        currAFreq = 0;
-                    }
+                    res = Math.max(dp[j][i][1], res);
+                    res = Math.max(dp[i][j][1], res);
                 }
             }
         }
 
-        return maxVariance;
+        return res;
     }
 }
+
+// class Solution {
+// public int largestVariance(String s) {
+// int[] freq = new int[26];
+// for (int i = 0; i < s.length(); i++)
+// freq[(int) (s.charAt(i) - 'a')]++;
+
+// int maxVariance = 0;
+// for (int a = 0; a < 26; a++) {
+// for (int b = 0; b < 26; b++) {
+// int remainingA = freq[a];
+// int remainingB = freq[b];
+// if (a == b || remainingA == 0 || remainingB == 0)
+// continue;
+
+// // run Kadanes Algorithm on each possible character pairs (A & B)
+// int currBFreq = 0, currAFreq = 0;
+// for (int i = 0; i < s.length(); i++) {
+// int c = (int) (s.charAt(i) - 'a');
+
+// if (c == b)
+// currBFreq++;
+// if (c == a) {
+// currAFreq++;
+// remainingA--;
+// }
+
+// if (currAFreq > 0)
+// maxVariance = Math.max(maxVariance, currBFreq - currAFreq);
+
+// if (currBFreq < currAFreq && remainingA >= 1) {
+// currBFreq = 0;
+// currAFreq = 0;
+// }
+// }
+// }
+// }
+
+// return maxVariance;
+// }
+// }
 // @lc code=end
