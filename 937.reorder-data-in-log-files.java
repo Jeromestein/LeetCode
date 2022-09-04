6f4e272e6fd950b874441608b4a0454a8ca1d620
Jerome <nietsemorej@gmail.com>
2022-09-04 15:40:08 -0400

    15ms, 24%, 44%.

--- 937.reorder-data-in-log-files.java
+++ 937.reorder-data-in-log-files.java
@@ -6,62 +6,43 @@
 
 // @lc code=start
 class Solution {
-    private static String LETTER_REGEX = "[a-z]+";
-    private static String DIGIT_REGEX = "[0-9]+";
-
     public String[] reorderLogFiles(String[] logs) {
-        // 1. judge letter/digit logs
-        // [identifier, contents]
-        List<List<String>> letterLogs = new ArrayList<>();
-        List<String> digitLogs = new ArrayList<>();
 
-        for (String log : logs) {
-            String[] str = log.split(" ", 2);
-            String identifier = str[0];
-            String contents = str[1];
-            // except the identifier
-            String[] words = contents.split(" ");
-            boolean isLetterLogs = true, isDigitLogs = true;
-            for (String word : words) {
-                if (!word.matches(LETTER_REGEX))
-                    isLetterLogs = false;
-                if (!word.matches(DIGIT_REGEX))
-                    isDigitLogs = false;
-            }
+        Comparator<String> myComp = new Comparator<String>() {
+            public int compare(String log1, String log2) {
+                // split each log into two parts: [identifier, contents]
+                String[] split1 = log1.split(" ", 2);
+                String[] split2 = log2.split(" ", 2);
 
-            if (isLetterLogs) {
-                letterLogs.add(new ArrayList<>());
-                int n = letterLogs.size();
-                letterLogs.get(n - 1).add(identifier);
-                letterLogs.get(n - 1).add(contents);
-            }
+                boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+                boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
 
-            if (isDigitLogs)
-                digitLogs.add(log);
-        }
+                // case 1: both logs are letter-logs
+                if (!isDigit1 && !isDigit2) {
+                    // first compare the content
+                    int contentCmp = split1[1].compareTo(split2[1]);
+                    if (contentCmp != 0)
+                        return contentCmp;
+                    else
+                        // logs of same content, compare the identifiers
+                        return split1[0].compareTo(split2[0]);
+                }
 
-        // 2. reorder: letter logs first, lexicographically; digti logs keep relative
-        // order
-        // defaultly lexicographical order
-        Collections.sort(letterLogs, new Comparator<List<String>>() {
-            public int compare(List<String> l1, List<String> l2) {
-                if (l1.get(1).compareTo(l2.get(1)) != 0)
-                    return l1.get(1).compareTo(l2.get(1));
+                // case 2: one of logs is digit-log
+                if (!isDigit1 && isDigit2)
+                    // the letter-log comes before digit-logs
+                    return -1;
+                else if (isDigit1 && !isDigit2)
+                    return 1;
                 else
-                    return l1.get(0).compareTo(l2.get(0));
+                    // case 3). both logs are digit-log
+                    // keep relative order
+                    return 0;
             }
-        });
+        };
 
-        String[] res = new String[letterLogs.size() + digitLogs.size()];
-        int i;
-        for (i = 0; i < letterLogs.size(); i++) {
-            res[i] = letterLogs.get(i).get(0) + " " + letterLogs.get(i).get(1);
-        }
-        for (int j = 0; j < digitLogs.size(); j++) {
-            res[i + j] = digitLogs.get(j);
-        }
-
-        return res;
+        Arrays.sort(logs, myComp);
+        return logs;
     }
 }
 // @lc code=end
