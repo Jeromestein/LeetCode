ac3fd72c0351784265fd800dc25ff8e4fcd956a8
Jerome <nietsemorej@gmail.com>
2022-09-05 15:44:35 -0400

    Add comments

--- 588.design-in-memory-file-system.java
+++ 588.design-in-memory-file-system.java
@@ -5,9 +5,12 @@
  */
 
 // @lc code=start
+// Unified Directory and File List
 public class FileSystem {
     class File {
-        boolean isfile = false;
+        boolean isFile = false;
+        // Use hashmap to store all files in current directory
+        // <file names, file>
         HashMap<String, File> files = new HashMap<>();
         String content = "";
     }
@@ -19,53 +22,64 @@ public class FileSystem {
     }
 
     public List<String> ls(String path) {
-        File t = root;
-        List<String> files = new ArrayList<>();
+        File curr = root;
         if (!path.equals("/")) {
-            String[] d = path.split("/");
-            for (int i = 1; i < d.length; i++) {
-                t = t.files.get(d[i]);
+            String[] levels = path.split("/");
+            for (int i = 1; i < levels.length; i++) {
+                curr = curr.files.get(levels[i]);
             }
-            if (t.isfile) {
-                files.add(d[d.length - 1]);
-                return files;
+            // If path is a file path,
+            // returns a list that only contains this file's name.
+            if (curr.isFile) {
+                List<String> res = new ArrayList<>();
+                res.add(levels[levels.length - 1]);
+                return res;
             }
         }
-        List<String> res_files = new ArrayList<>(t.files.keySet());
-        Collections.sort(res_files);
-        return res_files;
+        // If path is a directory path,
+        // returns the list of file and directory names in this directory
+        // lexicographically.
+        List<String> res = new ArrayList<>(curr.files.keySet());
+        Collections.sort(res);
+        return res;
     }
 
     public void mkdir(String path) {
-        File t = root;
-        String[] d = path.split("/");
-        for (int i = 1; i < d.length; i++) {
-            if (!t.files.containsKey(d[i]))
-                t.files.put(d[i], new File());
-            t = t.files.get(d[i]);
+        // Makes a new directory according to the given path.
+        // If the middle directories in the path do not exist,
+        // you should create them as well.
+        File curr = root;
+        String[] levels = path.split("/");
+        for (int i = 1; i < levels.length; i++) {
+            if (!curr.files.containsKey(levels[i]))
+                curr.files.put(levels[i], new File());
+            curr = curr.files.get(levels[i]);
         }
     }
 
     public void addContentToFile(String filePath, String content) {
-        File t = root;
-        String[] d = filePath.split("/");
-        for (int i = 1; i < d.length - 1; i++) {
-            t = t.files.get(d[i]);
+        File curr = root;
+        String[] levels = filePath.split("/");
+        for (int i = 1; i < levels.length - 1; i++) {
+            curr = curr.files.get(levels[i]);
         }
-        if (!t.files.containsKey(d[d.length - 1]))
-            t.files.put(d[d.length - 1], new File());
-        t = t.files.get(d[d.length - 1]);
-        t.isfile = true;
-        t.content = t.content + content;
+        // If filePath does not exist, creates that file containing given content.
+        if (!curr.files.containsKey(levels[levels.length - 1]))
+            curr.files.put(levels[levels.length - 1], new File());
+        // If filePath already exists, appends the given content to original content.
+        curr = curr.files.get(levels[levels.length - 1]);
+        curr.isFile = true;
+        curr.content = curr.content + content;
     }
 
     public String readContentFromFile(String filePath) {
-        File t = root;
-        String[] d = filePath.split("/");
-        for (int i = 1; i < d.length - 1; i++) {
-            t = t.files.get(d[i]);
+        // Returns the content in the file at filePath.
+        File curr = root;
+        String[] levels = filePath.split("/");
+        for (int i = 1; i < levels.length - 1; i++) {
+            curr = curr.files.get(levels[i]);
         }
-        return t.files.get(d[d.length - 1]).content;
+        return curr.files.get(levels[levels.length - 1]).content;
     }
 }
 
