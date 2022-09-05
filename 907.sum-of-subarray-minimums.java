ce53e95f1184366e211b32e7e0e66213625990db
Jerome <nietsemorej@gmail.com>
2022-09-05 11:21:08 -0400

    Add comments.

--- 907.sum-of-subarray-minimums.java
+++ 907.sum-of-subarray-minimums.java
@@ -6,33 +6,27 @@
 
 // @lc code=start
 class Solution {
-    private static final int MOD = 1000000007;
+    int MOD = (int) 1e9 + 7;
 
     private int getElement(int[] arr, int n, int i) {
-        if (i == -1 || i == n) {
-            return Integer.MIN_VALUE;
-        }
-        return arr[i];
+        return (i == -1 || i == n) ? Integer.MIN_VALUE : arr[i];
     }
 
     public int sumSubarrayMins(int[] arr) {
-        // 处理边界情况
-        if (arr == null || arr.length == 0) {
-            return 0;
-        }
         int n = arr.length;
         long ans = 0;
+        // Deque is faster than Stack
         Deque<Integer> stack = new LinkedList<>();
-        // 将下标-1和n作为两个哨兵元素，它们对应的元素为MIN_VALUE
-        // -1作为最左边界，n作为最右边界
+
+        // 2 sentinels: set arr[-1] & arr[n] as MIN_VALUE
         for (int i = -1; i <= n; i++) {
-            // 向左寻找第一个小于等于A[i]的元素
+            // Find left closest element, e <= A[i]
             while (!stack.isEmpty() && getElement(arr, n, stack.peek()) > getElement(arr, n, i)) {
-                // A[cur]就是之前思路中的A[i]，注意区分和上面代码的区别
-                // 对于每个出栈元素来说，i就是它们的右边界，而栈顶元素就是左边界
-                int cur = stack.pop();
-                // 计算贡献值
-                ans = (ans + (long) (cur - stack.peek()) * (i - cur) * arr[cur]) % MOD;
+                // for each stack.pop(),
+                // i is its right boundary and stack.peek() is left boundary.
+                int curr = stack.pop();
+                // calculate contribution
+                ans = (ans + (long) (curr - stack.peek()) * (i - curr) * arr[curr]) % MOD;
             }
             stack.push(i);
         }
