28ddc757b35518321bf10ea03d0dd7fb79eb3125
Jerome <nietsemorej@gmail.com>
2022-09-05 11:55:19 -0400

    43ms, 15%, 18%.

---
+++ 2104.sum-of-subarray-ranges.java
@@ -0,0 +1,47 @@
+import java.util.Deque;
+
+/*
+ * @lc app=leetcode id=2104 lang=java
+ *
+ * [2104] Sum of Subarray Ranges
+ */
+
+// @lc code=start
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long res = 0;
+
+        Deque<Integer> s = new LinkedList<>();
+        // res -= sumSubarrayMins
+        for (int i = -1; i <= n; i++) {
+            while (!s.isEmpty() && getElement1(nums, s.peek()) > getElement1(nums, i)) {
+                int currIdx = s.pop();
+                int left = s.peek(), right = i;
+                res -= (long) nums[currIdx] * (currIdx - left) * (right - currIdx);
+            }
+            s.push(i);
+        }
+
+        // res += sumSubarrayMaxs
+        for (int i = -1; i <= n; i++) {
+            while (!s.isEmpty() && getElement2(nums, s.peek()) < getElement2(nums, i)) {
+                int currIdx = s.pop();
+                int left = s.peek(), right = i;
+                res += (long) nums[currIdx] * (currIdx - left) * (right - currIdx);
+            }
+            s.push(i);
+        }
+
+        return res;
+    }
+
+    private int getElement1(int[] arr, int i) {
+        return (i == -1 || i == arr.length) ? Integer.MIN_VALUE : arr[i];
+    }
+
+    private int getElement2(int[] arr, int i) {
+        return (i == -1 || i == arr.length) ? Integer.MAX_VALUE : arr[i];
+    }
+}
+// @lc code=end
