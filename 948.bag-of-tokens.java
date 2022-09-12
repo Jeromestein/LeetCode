875e4050d9931a45abaa2e795469cea3f2737315
Jerome <nietsemorej@gmail.com>
2022-09-12 18:22:09 -0400

    4ms, 61%, 32%.

---
+++ 948.bag-of-tokens.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=948 lang=java
+ *
+ * [948] Bag of Tokens
+ */
+
+// @lc code=start
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int P) {
+        Arrays.sort(tokens);
+        int lo = 0, hi = tokens.length - 1;
+        int points = 0, ans = 0;
+        while (lo <= hi && (P >= tokens[lo] || points > 0)) {
+            while (lo <= hi && P >= tokens[lo]) {
+                P -= tokens[lo++];
+                points++;
+            }
+
+            ans = Math.max(ans, points);
+            if (lo <= hi && points > 0) {
+                P += tokens[hi--];
+                points--;
+            }
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
