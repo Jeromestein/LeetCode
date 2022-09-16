1f4a125243cf4296d1c53764329a47071e45fb7f
Jerome <nietsemorej@gmail.com>
2022-09-16 11:18:19 -0400

    Optimized space, m^2->m.

--- 1770.maximum-score-from-performing-multiplication-operations.java
+++ 1770.maximum-score-from-performing-multiplication-operations.java
@@ -9,23 +9,22 @@
 // Iterative Dynamic Programming:
 class Solution {
     public int maximumScore(int[] nums, int[] multipliers) {
-        // For Right Pointer
-        int n = nums.length;
-        // Number of Operations
-        int m = multipliers.length;
-        int[][] dp = new int[m + 1][m + 1];
+        int n = nums.length, m = multipliers.length;
+        int[] dp = new int[m + 1];
 
-        for (int i = 0; i <= m; i++)
-            Arrays.fill(dp[i], 0);
+        Arrays.fill(dp, 0);
 
         for (int op = m - 1; op >= 0; op--) {
+            int[] nextDp = dp.clone();
             for (int left = op; left >= 0; left--) {
-                dp[op][left] = Math.max(multipliers[op] * nums[left] + dp[op + 1][left + 1],
-                        multipliers[op] * nums[n - 1 - (op - left)] + dp[op + 1][left]);
+                // len(nums)=right-left+op+1
+                int right = n - 1 - (op - left);
+                dp[left] = Math.max(multipliers[op] * nums[left] + nextDp[left + 1],
+                        multipliers[op] * nums[right] + nextDp[left]);
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
 // @lc code=end
