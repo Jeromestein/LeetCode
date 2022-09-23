cc0640a134828ddcd315f8fd5c894c0a90b8d852
Jerome <nietsemorej@gmail.com>
2022-09-23 19:07:27 -0400

    Bitwise Operation, 96ms, 83%, 75%.

---
+++ 1680.concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1680 lang=java
+ *
+ * [1680] Concatenation of Consecutive Binary Numbers
+ */
+
+// @lc code=start
+class Solution {
+    public int concatenatedBinary(int n) {
+        final int MOD = 1000000007;
+        int length = 0; // bit length of addends
+        long result = 0; // long accumulator
+        for (int i = 1; i <= n; i++) {
+            // when meets power of 2, increase the bit length
+            if ((i & (i - 1)) == 0) {
+                length++;
+            }
+            result = ((result << length) | i) % MOD;
+        }
+        return (int) result;
+    }
+}
+// @lc code=end
