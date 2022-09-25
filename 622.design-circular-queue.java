7dc20bd9a9f82cf342e54fd421b83996586ea0e6
Jerome <nietsemorej@gmail.com>
2022-09-25 10:49:32 -0400

    Array, 3ms, 100%, 99%.

--- 622.design-circular-queue.java
+++ 622.design-circular-queue.java
@@ -6,27 +6,19 @@
 
 // @lc code=start
 
-class Node {
-    public int value;
-    public Node nextNode;
-
-    public Node(int value) {
-        this.value = value;
-        this.nextNode = null;
-    }
-}
-
 class MyCircularQueue {
 
-    private Node head, tail;
+    private int[] queue;
+    private int headIndex;
     private int count;
     private int capacity;
-    // Additional variable to secure the access of our queue
-    private ReentrantLock queueLock = new ReentrantLock();
 
     /** Initialize your data structure here. Set the size of the queue to be k. */
     public MyCircularQueue(int k) {
         this.capacity = k;
+        this.queue = new int[k];
+        this.headIndex = 0;
+        this.count = 0;
     }
 
     /**
@@ -34,24 +26,10 @@ class MyCircularQueue {
      * successful.
      */
     public boolean enQueue(int value) {
-        // ensure the exclusive access for the following block.
-        queueLock.lock();
-        try {
-            if (this.count == this.capacity)
-                return false;
-
-            Node newNode = new Node(value);
-            if (this.count == 0) {
-                head = tail = newNode;
-            } else {
-                tail.nextNode = newNode;
-                tail = newNode;
-            }
-            this.count += 1;
-
-        } finally {
-            queueLock.unlock();
-        }
+        if (this.count == this.capacity)
+            return false;
+        this.queue[(this.headIndex + this.count) % this.capacity] = value;
+        this.count += 1;
         return true;
     }
 
@@ -62,7 +40,7 @@ class MyCircularQueue {
     public boolean deQueue() {
         if (this.count == 0)
             return false;
-        this.head = this.head.nextNode;
+        this.headIndex = (this.headIndex + 1) % this.capacity;
         this.count -= 1;
         return true;
     }
@@ -71,16 +49,15 @@ class MyCircularQueue {
     public int Front() {
         if (this.count == 0)
             return -1;
-        else
-            return this.head.value;
+        return this.queue[this.headIndex];
     }
 
     /** Get the last item from the queue. */
     public int Rear() {
         if (this.count == 0)
             return -1;
-        else
-            return this.tail.value;
+        int tailIndex = (this.headIndex + this.count - 1) % this.capacity;
+        return this.queue[tailIndex];
     }
 
     /** Checks whether the circular queue is empty or not. */
@@ -93,6 +70,95 @@ class MyCircularQueue {
         return (this.count == this.capacity);
     }
 }
+
+// class Node {
+// public int value;
+// public Node nextNode;
+
+// public Node(int value) {
+// this.value = value;
+// this.nextNode = null;
+// }
+// }
+
+// class MyCircularQueue {
+
+// private Node head, tail;
+// private int count;
+// private int capacity;
+// // Additional variable to secure the access of our queue
+// private ReentrantLock queueLock = new ReentrantLock();
+
+// /** Initialize your data structure here. Set the size of the queue to be k.
+// */
+// public MyCircularQueue(int k) {
+// this.capacity = k;
+// }
+
+// /**
+// * Insert an element into the circular queue. Return true if the operation is
+// * successful.
+// */
+// public boolean enQueue(int value) {
+// // ensure the exclusive access for the following block.
+// queueLock.lock();
+// try {
+// if (this.count == this.capacity)
+// return false;
+
+// Node newNode = new Node(value);
+// if (this.count == 0) {
+// head = tail = newNode;
+// } else {
+// tail.nextNode = newNode;
+// tail = newNode;
+// }
+// this.count += 1;
+
+// } finally {
+// queueLock.unlock();
+// }
+// return true;
+// }
+
+// /**
+// * Delete an element from the circular queue. Return true if the operation is
+// * successful.
+// */
+// public boolean deQueue() {
+// if (this.count == 0)
+// return false;
+// this.head = this.head.nextNode;
+// this.count -= 1;
+// return true;
+// }
+
+// /** Get the front item from the queue. */
+// public int Front() {
+// if (this.count == 0)
+// return -1;
+// else
+// return this.head.value;
+// }
+
+// /** Get the last item from the queue. */
+// public int Rear() {
+// if (this.count == 0)
+// return -1;
+// else
+// return this.tail.value;
+// }
+
+// /** Checks whether the circular queue is empty or not. */
+// public boolean isEmpty() {
+// return (this.count == 0);
+// }
+
+// /** Checks whether the circular queue is full or not. */
+// public boolean isFull() {
+// return (this.count == this.capacity);
+// }
+// }
 
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
