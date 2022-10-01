6f936973bd52c0a64e2c18e57b89773e51b90590
Jerome <nietsemorej@gmail.com>
2022-10-01 19:00:59 -0400

    2ms, 70%, 45%.

---
+++ 91.decode-ways.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=91 lang=java
+ *
+ * [91] Decode Ways
+ */
+
+// @lc code=start
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        int twoBack = 1;
+        int oneBack = 1;
+        for (int i = 1; i < n; i++) {
+            int current = 0;
+            if (s.charAt(i) != '0') {
+                current = oneBack;
+            }
+            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                current += twoBack;
+            }
+
+            twoBack = oneBack;
+            oneBack = current;
+        }
+        return oneBack;
+    }
+}
+// @lc code=end
