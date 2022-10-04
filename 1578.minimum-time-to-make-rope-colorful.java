693894573d599ad60344bfe4aa175300d9071731
Jerome <nietsemorej@gmail.com>
2022-10-03 21:27:05 -0400

    36ms, 15%, 80%.

---
+++ 1578.minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1578 lang=java
+ *
+ * [1578] Minimum Time to Make Rope Colorful
+ */
+
+// @lc code=start
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        // totalTime: total time needed to make rope colorful;
+        // currMaxTime: maximum time of a balloon needed.
+        int totalTime = 0, currMaxTime = 0;
+
+        // For each balloon in the array:
+        for (int i = 0; i < colors.length(); ++i) {
+            // If this balloon is the first balloon of a new group
+            // set the currMaxTime as 0.
+            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
+                currMaxTime = 0;
+            }
+
+            // Increment totalTime by the smaller one.
+            // Update currMaxTime as the larger one.
+            totalTime += Math.min(currMaxTime, neededTime[i]);
+            currMaxTime = Math.max(currMaxTime, neededTime[i]);
+        }
+
+        // Return totalTime as the minimum removal time.
+        return totalTime;
+    }
+}
+// @lc code=end
