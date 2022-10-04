0385a66ff54a05dd4b2f200676e1e4d131b2b78f
Jerome <nietsemorej@gmail.com>
2022-10-04 14:01:37 -0400

    1ms, 43%, 63%.

---
+++ 660.remove-9.java
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=660 lang=java
+ *
+ * [660] Remove 9
+ */
+
+// @lc code=start
+class Solution {
+    public int newInteger(int n) {
+        return Integer.parseInt(Integer.toString(n, 9));
+    }
+}
+// @lc code=end
