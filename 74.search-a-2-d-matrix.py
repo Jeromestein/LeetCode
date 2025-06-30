3a8e43d2fe8744db92c78cd633396f44eded2865
Jerome <nietsemorej@gmail.com>
2025-06-30 13:51:38 -0700

    Refactor variable names in searchMatrix method of LeetCode problem 74: Change 'l' and 'r' to 'left' and 'right' for improved readability in binary search implementation.

--- 74.search-a-2-d-matrix.py
+++ 74.search-a-2-d-matrix.py
@@ -9,17 +9,17 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
-        l, r = 0, m*n-1
+        left, right = 0, m*n-1
 
-        while l <= r:
-            mid = (l+r)//2
+        while left <= right:
+            mid = (left+right)//2
             row, col = mid//n, mid % n
             if matrix[row][col] == target:
                 return True
             elif matrix[row][col] < target:
-                l = mid+1
+                left = mid+1
             else:
-                r = mid-1
+                right = mid-1
 
         return False
 
