50a59f5ba3e8d8bcc8032acc198c08c321f6b455
Jerome <nietsemorej@gmail.com>
2025-07-02 15:50:15 -0700

    Add new Python file for LeetCode problem 215: Implement solution for finding the Kth largest element in an array using the quick select algorithm.

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=215 lang=python3
+#
+# [215] Kth Largest Element in an Array
+#
+
+# @lc code=start
+class Solution:
+    def findKthLargest(self, nums, k):
+        def quick_select(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+
+            for num in nums:
+                if num > pivot:
+                    left.append(num)
+                elif num < pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)
+            
+            if k <= len(left):
+                return quick_select(left, k)
+            
+            if len(left) + len(mid) < k:
+                return quick_select(right, k - len(left) - len(mid))
+            
+            return pivot
+        
+        return quick_select(nums, k)
+# @lc code=end
+
