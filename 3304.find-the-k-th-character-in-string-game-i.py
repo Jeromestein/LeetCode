539979e93ada92d7d09e49a44df66963d61da876
Jerome <nietsemorej@gmail.com>
2025-07-03 14:23:14 -0700

    Add new Python file for LeetCode problem 3304: Implement initial structure for finding the K-th character in a string.

---
+++ 3304.find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=3304 lang=python3
+#
+# [3304] Find the K-th Character in String Game I
+#
+
+# @lc code=start
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        
+
+
+        
+# @lc code=end
+
