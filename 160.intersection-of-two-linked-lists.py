a9391745555353ab04af11cc904d91cd128fb289
Jerome <nietsemorej@gmail.com>
2025-07-04 15:23:07 -0700

    Add new Python file for LeetCode problem 160: Implement solution for finding the intersection of two linked lists using a two-pointer technique.

---
+++ 160.intersection-of-two-linked-lists.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=160 lang=python3
+#
+# [160] Intersection of Two Linked Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        ptrA, ptrB=headA, headB
+        while ptrA!=ptrB:
+            if ptrA:
+                ptrA=ptrA.next
+            else:
+                ptrA=headB
+            
+            if ptrB:
+                ptrB=ptrB.next
+            else:
+                ptrB=headA
+
+        return ptrA
+
+
+# @lc code=end
+
