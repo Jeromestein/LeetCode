f009675c4a35a4356d1adf8f28a66a23356afabd
Jerome <nietsemorej@gmail.com>
2025-07-25 11:39:00 -0700

    feat: add solution for LeetCode problem 206: Implement reverse linked list

---
+++ 206.reverse-linked-list.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=206 lang=python3
+#
+# [206] Reverse Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from hmac import new
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
+
+
+# @lc code=end
