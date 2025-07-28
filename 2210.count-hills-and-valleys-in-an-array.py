b79368fb1ed15e2e18c45e5819ce19b2b5fde3cc
Jerome <nietsemorej@gmail.com>
2025-07-28 10:28:06 -0700

    Add solutions for LeetCode problems 2044, 2210, and 3480; update markdown files for problem descriptions and examples; implement sliding window and recursive approaches for optimal solutions.

---
+++ 2210.count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=2210 lang=python3
+#
+# [2210] Count Hills and Valleys in an Array
+#
+
+# @lc code=start
+
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        n = len(nums)
+        count, left = 0, 0
+        for i in range(1, n - 1):
+            if nums[i] != nums[i + 1]:
+                if (nums[i] > nums[left] and nums[i] > nums[i + 1]) or (
+                    nums[i] < nums[left] and nums[i] < nums[i + 1]
+                ):
+                    count += 1
+                left = i
+
+        return count
+
+
+# @lc code=end
