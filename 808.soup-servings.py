751a6e8edca67217510d30073c85cdb079395af3
Jerome <nietsemorej@gmail.com>
2025-08-09 08:48:31 -0700

    feat: add initial implementation for LeetCode problem 808: Soup Servings

---
+++ 808.soup-servings.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=808 lang=python3
+#
+# [808] Soup Servings
+#
+
+# @lc code=start
+class Solution:
+    def soupServings(self, n: int) -> float:
+        
+# @lc code=end
+
