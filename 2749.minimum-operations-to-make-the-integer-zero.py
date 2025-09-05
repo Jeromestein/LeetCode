2f40d51fa213604f0fceb39dd1546c8fe216e34d
Jerome <nietsemorej@gmail.com>
2025-09-05 09:54:16 -0700

    Add solution for LeetCode problem 2749: Minimum Operations to Make the Integer Zero and update related markdown documentation

--- 2749.minimum-operations-to-make-the-integer-zero.py
+++ 2749.minimum-operations-to-make-the-integer-zero.py
@@ -5,8 +5,18 @@
 #
 
 # @lc code=start
+
+
 class Solution:
     def makeTheIntegerZero(self, num1: int, num2: int) -> int:
-        
-# @lc code=end
+        k = 1
+        while True:
+            x = num1 - num2 * k
+            if x < k:
+                return -1
+            if k >= x.bit_count():
+                return k
+            k += 1
 
+
+# @lc code=end
