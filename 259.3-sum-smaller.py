4afe9df48e8b5cc0967fc61bfcc16a8d39ad669b
Jerome <nietsemorej@gmail.com>
2025-11-17 11:30:48 -0800

    Implement solution for LeetCode problem 259: 3Sum Smaller with detailed logic for counting valid triplets.

--- 259.3-sum-smaller.py
+++ 259.3-sum-smaller.py
@@ -5,8 +5,29 @@
 #
 
 # @lc code=start
-class Solution:
-    def threeSumSmaller(self, nums: List[int], target: int) -> int:
-        
-# @lc code=end
 
+
+class Solution:
+    def threeSumSmaller(self, nums, target):
+        nums.sort()
+        total = 0
+        for i in range(len(nums) - 2):
+            total += self.twoSumSmaller(nums[i + 1 :], target - nums[i])
+
+        return total
+
+    def twoSumSmaller(self, nums, target):
+        count = 0
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += right - left
+                left += 1
+            else:
+                right -= 1
+
+        return count
+
+
+# @lc code=end
